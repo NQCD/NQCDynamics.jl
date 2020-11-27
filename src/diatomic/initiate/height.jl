54f9a565936de9c00c4a5cb2fe978d341abc1732
clbox <connor.box@warwick.ac.uk>
2020-11-27 12:42:47 +0000

    height

---
+++ src/diatomic/initiate/height.jl
@@ -0,0 +1,15 @@
+#Displace diatomic to specified height
+#Not sure if this should be apart of it
+
+
+function height(system,positions,height)
+
+    COM,rm = calc_COM(system)
+
+    COM[2] = height_original
+
+    displacement = height - height_original
+
+    positions(2,:) += displacement
+
+end
