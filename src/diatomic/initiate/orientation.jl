b88819838f7b94d3f176d246d84c66910e2f1728
clbox <connor.box@warwick.ac.uk>
2020-11-27 12:39:29 +0000

    orientation base

--- src/diatomic/initiate/orientation.jl
+++ src/diatomic/initiate/orientation.jl
@@ -1,4 +1,10 @@
-#Randomly select initial orientation around COM
+#Randomly select rotate around COM
 #In future allow specific range of angles to be selected e.g 0-60 degrees
 
-function orient()
+function orient(system,positions)
+
+    θ =  cos^-1(2*rand1-1)
+    ϕ = 2*π*rand2
+    χ = 2*π*rand3
+    
+end
