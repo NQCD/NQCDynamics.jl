e4ad5316ae0fc4ac836a7516341228d32e410c9f
James <james.gardner1421@gmail.com>
2020-11-27 20:08:09 +0000

    Copy over models from NonAdiabaticRingPolymers.jl

---
+++ src/Models/Analytic/analytic_models/harmonic.jl
@@ -0,0 +1,17 @@
+export Harmonic
+
+"""
+The 1-state harmonic model
+"""
+struct Harmonic <: AnalyticModel
+
+    @add_standard_fields
+
+    function Harmonic(mass, omega, r_0)
+
+        V(q) = 0.5 * mass * omega ^ 2 * (q - r_0) ^2
+        D(q) = mass * omega ^ 2 * (q - r_0)
+
+        new(V, D, zero_hermitian, zero_hermitian)
+    end
+end
