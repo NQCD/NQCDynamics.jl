27309325a97fa3d643c0c516aa31535385c77460
GARDNER <james.gardner1421@gmail.com>
2020-12-03 13:38:33 +0000

    Optimise descriptor creation
    Big speedup from removing the pyinit()
    
    Small speedup from inplace updating. But this would become more significant when removing the redundant updating of constant quantities.

--- src/Models/ML/test.jl
+++ src/Models/ML/test.jl
@@ -1,17 +1,13 @@
-#this is just for testing and copied from the examples of MDPrePostProcessing
 using NonadiabaticMolecularDynamics.Atoms
 using NonadiabaticMolecularDynamics.Systems
 using NonadiabaticMolecularDynamics.Models
 using NonadiabaticMolecularDynamics.Models.ML
 using NonadiabaticMolecularDynamics.Models.ML_descriptor
-#include(joinpath(pwd(),"src","Models","ML","ML.jl"))
 using Unitful
 using UnitfulAtomic
-using Revise
 using PeriodicTable
+using BenchmarkTools
 
-
-#test run
 model_path=joinpath(string(pwd()),"examples","ML_testfiles")
 
 x = [1.0, 0.0, 0.0]
@@ -21,32 +17,14 @@ z = [0.0, 0.0, 1.0]
 a = PeriodicCell([x y z])
 set_periodicity!(a, [false, true, false])
 p = AtomicParameters(a, [:H, :H])
-system = System(p, Models.Analytic.Free())
 R = [5.08124009 0.94499003 8.42028042; 4.72585008 0.70419003 7.57972028]
-#init ML
+R = [5.08124009, 0.94499003, 8.42028042, 4.72585008, 0.70419003, 7.57972028]
+
 property_dict = Dict("energy" => true, "forces" => true)
 model, model_args, force_mask = initialize_MLmodel(model_path, p)
 
-#init schnet_descriptor
-#environment_provider=env_init(model_args)
+@btime julia2schnet($p, $R, $model_args) # 751 μs, 450 allocations
+inputs = Dict{String, PyObject}()
+@btime update_schnet_input!($inputs, $p, $R, $model_args) # 727 μs, 391 allocations
 
-@time begin
-schnet_input=julia2schnet(p,R,model_args)
-schnet_outputs=model(schnet_input)
-#60ms
-
-end
-@time begin
-t=1000
-macro run_prediction(n,exp)
-    quote
-        for i = 1:$(esc(n))
-            $(esc(exp))
-        end
-    end
-end
-
-#@run_prediction(t,(model(schnet_input)))
-end
-print(schnet_outputs)
-print(schnet_input)
+@btime $model($inputs) # 5.78 ms, 47 allocations
