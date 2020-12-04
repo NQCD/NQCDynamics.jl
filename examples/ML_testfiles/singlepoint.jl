79a9a75a7b2da6c8f22dd7d4007fade32dacafe5
juliawestermayr <julia.westermayr@univie.ac.at>
2020-12-04 17:48:29 +0000

    added some files

---
+++ examples/ML_testfiles/singlepoint.jl
@@ -0,0 +1,21 @@
+using NonadiabaticMolecularDynamics.IO
+using NonadiabaticMolecularDynamics.Models.ML
+using PyCall
+ase = pyimport("ase")
+model_path = "examples/ML_testfiles"
+atoms, phasespace = read_system("examples/ML_testfiles/H2.xyz")
+model = SchNetPackModel(model_path, atoms)
+# model 1 : H2 in gas pahse
+R = ase.io.read(joinpath(model_path,"H2.xyz")).get_positions()
+R= reshape(R,length(R))
+energy = model.get_V0(R)
+print(energy)
+
+
+# model 1 : H2 in gas pahse
+atoms2, phasespace2 = read_system("examples/ML_testfiles/H2_Cu(100).xyz")
+model2 = SchNetPackModel(model_path, atoms2)
+R2 = ase.io.read(joinpath(model_path,"H2_Cu(100).xyz")).get_positions()
+R2= reshape(R2,length(R2))
+energy2 = model2.get_V0(R2)
+print(energy2)
