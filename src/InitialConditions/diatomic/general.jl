50303ecfc3d67aa2d83f77962c215da7aaa70aa8
clbox <connor.box@warwick.ac.uk>
2020-12-07 11:22:40 +0000

    getting there

--- src/InitialConditions/diatomic/general.jl
+++ src/InitialConditions/diatomic/general.jl
@@ -10,24 +10,13 @@ function calc_COM_momenta(P :: momenta)
     return COM_P
 end
 
-function energy(H,T,V)
-    # CALCULATE POTENTIAL, KINETIC AND TOTAL ENERGY OF THE
-    # MOLECULAR SYSTEM
+function calc_diatomic_energy(bondlength=R)
+    # wrapper function to calc diatomic potential and kinetic energy
+    # with bondlength R and height from surface Z
+    height = 9 #large enough to minimize interaction, small enough to be in training
+                #space
 
-    T=0.0
-    V=0.0
-  
-    CALL POT0(NATOMS,VV)
-    #  ADD VZERO TO THE POTENTIAL ENERGY
-    V=VV+VZERO
-    #        CALCULATE KINETIC ENERGY
-    J=1
-    DO I=1,NATOMS
-    T=T+(P(J)**2+P(J+1)**2+P(J+2)**2)/2.0/W(I)
-    J=J+3
-    ENDDO
-
-    H=T+V
+    return H, T, V
 end
 
 function calc_reduced_mass()
