7a5ddd890756e61e70d2327be3a8a21b1e65c014
James Gardner <james.gardner1421@gmail.com>
2020-12-07 18:23:58 +0000

    Add new structures for positions and momenta
    This can later be packaged into more complex structures such as the phasespace or other similar types

---
+++ examples/positions_and_momenta.jl
@@ -0,0 +1,20 @@
+using NonadiabaticMolecularDynamics
+
+n_DoF = 3
+n_atoms = 20
+
+# Can construct using matrix with each atom as columns 
+R = Positions(rand(n_DoF, n_atoms))
+P = Momenta(rand(n_DoF, n_atoms))
+
+# Alternatively, construct with array of arrays
+R = Positions([rand(n_DoF) for i=1:n_atoms])
+P = Momenta([rand(n_DoF) for i=1:n_atoms])
+
+R[:, 5] # Access atom 5
+R[5] # Access atom 5
+P[:, 5] # Access atom 5
+P[5] # Access atom 5
+
+# Standard array operations should work
+R + P
