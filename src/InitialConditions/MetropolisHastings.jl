35a6e52ba547aac07ac69e73616ad826a619dead
James Gardner <james.gardner1421@gmail.com>
2020-12-10 15:59:19 +0000

    Add simple periodic boundaries for monte carlo sampling

--- src/InitialConditions/MetropolisHastings.jl
+++ src/InitialConditions/MetropolisHastings.jl
@@ -43,6 +43,7 @@ end
 function perform_monte_carlo_step!(system::System{Classical, T}, Rᵢ::Matrix{T}, Rₚ::Matrix{T},
     energy::Vector{T}, Δ::T, output::MonteCarloOutput{T}, i::Integer) where {T<:AbstractFloat}
     propose_move!(Rᵢ, Rₚ, Δ, n_DoF(system), n_atoms(system))
+    apply_cell_boundaries!(system.atomic_parameters.cell, Rₚ, n_atoms(system), n_DoF(system))
     energy[2] = Electronics.evaluate_potential(system.model, Rₚ)
     assess_proposal!(energy, Rₚ, Rᵢ, system.temperature, output)
     write_output!(output.R[i], Rᵢ)
@@ -53,6 +54,29 @@ function propose_move!(Rᵢ::Matrix{T}, Rₚ::Matrix{T}, Δ::T, n_DoF::Integer, 
     atom = sample(1:n_atoms)
     Rₚ[:,atom] .+= (rand(n_DoF) .- 0.5) .* Δ / sqrt(n_DoF)
 end
+
+"""
+    apply_cell_boundaries!(cell::PeriodicCell, R::Matrix{<:AbstractFloat}, n_atoms::Integer, n_DoF::Integer)
+    
+Apply simple periodic boundaries 
+
+This works for cubic cells only.
+"""
+function apply_cell_boundaries!(cell::PeriodicCell, R::Matrix{<:AbstractFloat}, n_atoms::Integer, n_DoF::Integer)
+    cell_vectors = austrip.(cell.vectors)
+    for i=1:n_atoms
+        for j=1:n_DoF # Loop ordering is very inefficient, don't need to check every time
+            if cell.periodicity[j] # If periodic in this dimension
+                if R[j,i] > cell_vectors[j,j]
+                    R[j,i] -= cell_vectors[j,j]
+                elseif R[j,i] < 0
+                    R[j,i] += cell_vectors[j,j]
+                end
+            end
+        end
+    end
+end
+apply_cell_boundaries!(cell::InfiniteCell, R, n_atoms, n_DoF) = nothing
 
 function assess_proposal!(energy::Vector{T}, Rₚ::Matrix{T}, Rᵢ::Matrix{T}, temperature::T, output::MonteCarloOutput{T}) where {T<:AbstractFloat}
     if acceptance_probability(energy[2], energy[1], temperature) > rand()
