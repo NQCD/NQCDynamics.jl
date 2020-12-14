4994b92649eb3054a1112208c767db25a11dcfd3
James Gardner <james.gardner1421@gmail.com>
2020-12-14 10:24:43 +0000

    Add field to RingPolymerParameters
    New field contains a vector of the nuclei that are "quantum".
    This allows classical treatment of heavy nuclei and a ring polymer treatment of light nuclei.

--- test/ring_polymers.jl
+++ test/ring_polymers.jl
@@ -2,13 +2,20 @@ using Test
 using NonadiabaticMolecularDynamics
 using LinearAlgebra
 
-@test_nowarn Systems.RingPolymerParameters{Float64}(10, 1)
-@test_nowarn Systems.RingPolymerParameters{Float64}(1, 1)
-@test_nowarn Systems.RingPolymerParameters{Float64}(2, 1)
+@test_nowarn Systems.RingPolymerParameters{Float64}(10, 1, 1)
+@test_nowarn Systems.RingPolymerParameters{Float64}(1, 1, 1)
+@test_nowarn Systems.RingPolymerParameters{Float64}(2, 1, 1)
+rp = Systems.RingPolymerParameters{Float64}(10, 1.0, vcat(fill(:C, 3), fill(:H, 5)), [:H])
+@test rp.quantum_atoms == [4, 5, 6, 7, 8]
+rp = Systems.RingPolymerParameters{Float64}(10, 1.0, 10)
+@test rp.quantum_atoms == collect(1:10)
 
-rp = Systems.RingPolymerParameters(10, 1.0)
 @test sort(eigvals(rp.springs)) ≈ sort(rp.normal_mode_springs)
 
-atoms = Systems.AtomicParameters(Systems.InfiniteCell(), fill(:H, 10))
-@test_nowarn Systems.RingPolymerSystem(atoms, Free(), 10, 10)
-system = Systems.RingPolymerSystem(atoms, Free(), 10, 10)
+atoms = Systems.AtomicParameters(Systems.InfiniteCell(), vcat(fill(:H, 10), :O))
+Systems.RingPolymerSystem(atoms, Models.Analytic.Free(), 10, 10)
+system = Systems.RingPolymerSystem(atoms, Models.Analytic.Free(), 10, 10)
+@test system.ring_polymer.quantum_atoms == collect(1:11)
+system = Systems.RingPolymerSystem(atoms, Models.Analytic.Free(), 10, 10, quantum_nuclei=[:H])
+@test system.ring_polymer.quantum_atoms == collect(1:10)
+
