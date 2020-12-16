5b33481fd002f1ab54a0921b698a3404bd55a537
James Gardner <james.gardner1421@gmail.com>
2020-12-16 11:05:33 +0000

    Change layout of ring polymer data

--- src/Dynamics/DynamicsVariables/classical_hamiltonians.jl
+++ src/Dynamics/DynamicsVariables/classical_hamiltonians.jl
@@ -6,14 +6,14 @@ function evaluate_configurational_energy(system::System, R::Matrix)
     Electronics.evaluate_potential(system.model, R)
 end
 
-function evaluate_configurational_energy(system::RingPolymerSystem, R::RingPolymerArray)
+function evaluate_configurational_energy(system::RingPolymerSystem, R::Array{T, 3}) where {T}
     get_spring_energy(system, R) + get_potential_energy(system, R)
 end
 
-function get_potential_energy(system::RingPolymerSystem, R::RingPolymerArray)
+function get_potential_energy(system::RingPolymerSystem, R::Array{T, 3}) where {T}
     V = 0.0
-    for i=1:n_beads(system)
-        V += Electronics.evaluate_potential(system.model, R.x[i])
+    @views for i=1:n_beads(system)
+        V += Electronics.evaluate_potential(system.model, R[:,:,i])
     end
     V
 end
@@ -25,18 +25,18 @@ Calculate the ring polymer spring potential.
 
 I don't like having to write these loops but it seems faster than any alternative.
 """
-function get_spring_energy(system::RingPolymerSystem, R::RingPolymerArray)
+function get_spring_energy(system::RingPolymerSystem, R::Array{T, 3}) where {T}
     E = 0.0
     for bead=1:n_beads(system)-1
         for i in system.ring_polymer.quantum_atoms # Only for quantum nuclei
             for j=1:n_DoF(system)
-                E += masses(system)[i] * (R.x[bead][j, i] - R.x[bead+1][j, i])^2
+                E += masses(system)[i] * (R[j, i, bead] - R[j, i, bead+1])^2
             end
         end
     end
     for i in system.ring_polymer.quantum_atoms
         for j=1:n_DoF(system)
-            E += masses(system)[i] * (R.x[n_beads(system)][j, i] - R.x[1][j, i])^2
+            E += masses(system)[i] * (R[j, i, n_beads(system)] - R[j, i, 1])^2
         end
     end
     E * system.ring_polymer.ω_n^2/2
