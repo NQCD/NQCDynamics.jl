1a79a21cc1e12ca0cc085d43ebde2e820917bad9
James Gardner <james.gardner1421@gmail.com>
2020-12-16 12:37:36 +0000

    Ring polymer output

--- src/IO/IO.jl
+++ src/IO/IO.jl
@@ -33,15 +33,15 @@ function create_ase_atoms(atoms::AtomicParameters, R::Matrix{<:AbstractFloat})
     ase = pyimport("ase")
     if hasproperty(atoms.cell, :periodicity)
         return ase.Atoms(
-        positions=ustrip.(u"Å", R'u"bohr"),
-        cell=ustrip.(u"Å", atoms.cell.vectors),
-        symbols=string.(atoms.atom_types),
-        pbc=atoms.cell.periodicity)
+            positions=ustrip.(u"Å", R'u"bohr"),
+            cell=ustrip.(u"Å", atoms.cell.vectors),
+            symbols=string.(atoms.atom_types),
+            pbc=atoms.cell.periodicity)
     else
         return ase.Atoms(
             positions=ustrip.(u"Å", R'u"bohr"),
             symbols=string.(atoms.atom_types))
-end
+    end
 end
 
 function write_trajectory(file_name::String, solution::DESolution, p::AtomicParameters)
@@ -59,6 +59,16 @@ end
 function write_trajectory(file_name::String, positions::Vector{Matrix{T}}, p::AtomicParameters) where {T}
     ase = pyimport("ase")
     trajectory = create_ase_atoms.(Ref(p), positions)
+
+    ase.io.write(file_name, trajectory)
+end
+
+function write_trajectory(file_name::String, positions::Vector{Array{T, 3}}, p::AtomicParameters) where {T}
+    ase = pyimport("ase")
+    trajectory = []
+    for config in positions
+        push!(trajectory, sum([create_ase_atoms(p, config[:,:,i]) for i=1:size(positions[1])[3]]))
+    end
 
     ase.io.write(file_name, trajectory)
 end
