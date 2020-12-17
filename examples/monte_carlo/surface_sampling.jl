65635486c72b0e3c687c588678c1a7ab0cf98865
James Gardner <james.gardner1421@gmail.com>
2020-12-17 11:23:17 +0000

    RIng polymer sampling

--- examples/monte_carlo/surface_sampling.jl
+++ examples/monte_carlo/surface_sampling.jl
@@ -16,9 +16,10 @@ atoms, positions = extract_parameters_and_positions(slab)
 model = Models.PdH(atoms.atom_types, atoms.cell, 10.0)
 
 Δ = Dict([(:Pd, 0.5), (:H, 0.5)])
-sys = System{MonteCarlo}(atoms, model, 300u"K", Δ; passes=1000, fix=collect(1:6))
+sys = RingPolymerSystem{MonteCarlo}(atoms, model, 30u"K", 10, Δ; passes=100, fix=collect(1:6), quantum_nuclei=[:H])
 
-output = InitialConditions.run_monte_carlo_sampling(sys, positions)
+R = cat([positions for i=1:10]..., dims=3)
+output = InitialConditions.run_monte_carlo_sampling(sys, R)
 
 @show output.acceptance
 write_trajectory("sampling.xyz", output.R, atoms)
