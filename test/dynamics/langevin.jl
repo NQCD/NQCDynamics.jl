e3b21250f8661539d26c7a5d26b9f50db487ab52
smjanke <4113052+smjanke@users.noreply.github.com>
2020-12-21 13:50:54 +0000

    Add IESH_dynamics.jl to DynamicsMethods; bugfix langein.jl

--- test/dynamics/langevin.jl
+++ test/dynamics/langevin.jl
@@ -1,9 +1,10 @@
+push!(LOAD_PATH, pwd())
 using Test
 using NonadiabaticMolecularDynamics
 using Unitful
 
 @test Langevin{Float64}(1.0) == Langevin(1)
 
-atomic_parameters = Atoms.AtomicParameters(InfiniteCell(), [:H])
+atomic_parameters = Atoms.AtomicParameters(Atoms.InfiniteCell(), [:H])
 System{Langevin}(atomic_parameters, Models.Analytic.Free(), 10u"K", 1)
 
