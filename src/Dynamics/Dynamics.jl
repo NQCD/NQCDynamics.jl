e422df737425f7012c8dd3af9f681215c7f284c2
James Gardner <james.gardner1421@gmail.com>
2020-12-22 10:17:38 +0000

    Remove commented code

--- src/Dynamics/Dynamics.jl
+++ src/Dynamics/Dynamics.jl
@@ -1,17 +1,5 @@
 module Dynamics
 
-# using ..Electronics
-# using ..Systems
-
-# using Reexport
-# using DiffEqBase
-
-# include("DynamicsVariables/DynamicsVariables.jl")
-# @reexport using .DynamicsVariables
-
-# include("DynamicsMethods/DynamicsMethods.jl")
-# @reexport using .DynamicsMethods
-
 using ..NonadiabaticMolecularDynamics
 
 abstract type Method end
