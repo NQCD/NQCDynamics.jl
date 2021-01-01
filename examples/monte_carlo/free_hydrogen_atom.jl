6059de6062ed84e7ae4a91828cc0ffde16c55b31
James Gardner <james.gardner1421@gmail.com>
2021-01-01 17:48:54 +0000

    Refactor monte carlo sampling
    Now optional number of moves for the ring polymer normal modes so the sampling is more flexible.

--- examples/monte_carlo/free_hydrogen_atom.jl
+++ examples/monte_carlo/free_hydrogen_atom.jl
@@ -10,18 +10,17 @@ using Unitful
 using Plots
 
 model = Models.Free()
-cell = InfiniteCell{Float64}()
 atoms = Atoms{Float64}([:H])
 
 Δ = Dict([(:H, 1.0)]) # This is for the centroid mode and does nothing here as the centroid is fixed.
-beads = 100
-monte_carlo = InitialConditions.MonteCarlo{Float64}(Δ, length(atoms), 300, [1])
-sim = RingPolymerSimulation(1, 30u"K", cell, atoms, model, monte_carlo, beads)
+beads = 10
+monte_carlo = InitialConditions.PathIntegralMonteCarlo{Float64}(Δ, length(atoms), 300, [1], 0.5, beads)
+sim = RingPolymerSimulation(atoms, model, Dynamics.Classical(), beads; temperature=30u"K")
 
 positions = reshape(zeros(3), 3, 1) # Create zero coordinates
 R = cat([positions for i=1:beads]..., dims=3) # Copy coordinates for every bead
-output = InitialConditions.run_monte_carlo_sampling(sim, R)
+output = InitialConditions.run_monte_carlo_sampling(sim, monte_carlo, R)
 
 @show output.acceptance
-write_trajectory("sampling.xyz", cell, atoms, output.R)
+write_trajectory("sampling.xyz", InfiniteCell{Float64}(), atoms, output.R)
 plot(output.energy)
--- examples/monte_carlo/harmonic_sampling.jl
+++ examples/monte_carlo/harmonic_sampling.jl
@@ -3,14 +3,13 @@ using Unitful
 using Plots
 
 atoms = Atoms{Float64}(fill(:H, 5))
-cell = InfiniteCell{Float64}()
 model = Models.Harmonic()
 
 Δ = Dict([(:H, 0.3)])
 monte_carlo = InitialConditions.MonteCarlo{Float64}(Δ, length(atoms), 1000, Int[])
-sim = Simulation(1, 300u"K", cell, atoms, model, monte_carlo)
+sim = Simulation(atoms, model, Dynamics.Classical(); DoFs=1, temperature=300u"K")
 
-output = InitialConditions.run_monte_carlo_sampling(sim, zeros(1, length(sim.atoms)))
+output = InitialConditions.run_monte_carlo_sampling(sim, monte_carlo, zeros(1, length(sim.atoms)))
 
 @show output.acceptance
 plot(vcat(output.R...))
