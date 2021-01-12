81699305354b9416c82f9c7e48aa80736b41a46d
James Gardner <james.gardner1421@gmail.com>
2021-01-12 18:23:29 +0000

    Improve NRPMD performance

--- examples/dynamics/fssh_and_nrpmd_1D.jl
+++ examples/dynamics/fssh_and_nrpmd_1D.jl
@@ -14,7 +14,7 @@ plot(solution.t, [real(Dynamics.get_density_matrix(u)[1,1]) for u in solution.u]
 plot!(solution.t, [real(Dynamics.get_density_matrix(u)[2,2]) for u in solution.u], label="σ[2,2]")
 plot!(solution.t, [u.state for u in solution.u].-1, label="current surface")
 
-nrpmd_sim = RingPolymerSimulation(atoms, Models.TullyModelOne(), Dynamics.NRPMD(), 1; DoFs=1)
+nrpmd_sim = RingPolymerSimulation(atoms, Models.TullyModelOne(), Dynamics.NRPMD{Float64}(2), 1; DoFs=1)
 nrpmd_z = Dynamics.RingPolymerMappingPhasespace(R, P, 1, 2, 1)
 
 nrpmd_solution = Dynamics.run_trajectory(nrpmd_z, (0.0, 2500.0), nrpmd_sim)
