ef8b26e6407c67ae91e885cd83d2f08b11ac4a0d
smjanke <4113052+smjanke@users.noreply.github.com>
2021-01-27 22:08:40 +0000

    Investigated hopping probability

--- src/Dynamics/iesh.jl
+++ src/Dynamics/iesh.jl
@@ -1,8 +1,7 @@
 export SurfaceHoppingPhasespace
 export IESH
-export iesh_callback
-
-#For now, this is a copy of FSSH
+export IESH_callback
+using Revise
 
 struct IESH{T} <: Method
     nonadiabatic_coupling::Matrix{Matrix{T}}
@@ -31,25 +30,33 @@ function SurfaceHoppingPhasespace(R::Matrix{T}, P::Matrix{T}, σ::Matrix{Complex
     SurfaceHoppingPhasespace{T}(ArrayPartition(R, P, σ), state)
 end
 
+# construct the density matrix
 function SurfaceHoppingPhasespace(R::Matrix{T}, P::Matrix{T}, n_states::Integer, state::Integer) where {T}
     σ = zeros(Complex{T}, n_states, n_states)
     σ[state, state] = 1
+#    println(σ)
     SurfaceHoppingPhasespace(R, P, σ, state)
 end
 
 get_density_matrix(z::SurfaceHoppingPhasespace) = z.x.x[3]
 
+# This belongs to run_trajectory.
+# It first follows motion! and then iesh_callback
 function create_problem(u0::SurfaceHoppingPhasespace, tspan::Tuple, sim::AbstractSimulation{<:IESH})
-    ODEProblem(motion!, u0, tspan, sim; callback=iesh_callback)
+    ODEProblem(motion!, u0, tspan, sim; callback=IESH_callback)
 end
 
 function motion!(du::SurfaceHoppingPhasespace, u::SurfaceHoppingPhasespace, sim::Simulation{<:IESH}, t)
-    set_velocity!(du, u, sim)
-    update_electronics!(sim, u)
-    set_force!(du, u, sim)
-    set_density_matrix_derivative!(du, u, sim)
+    set_velocity!(du, u, sim)  # classical.jl momenta/(atom_mass) v = p/m 
+    update_electronics!(sim, u) # The next three routines 
+    set_force!(du, u, sim) # 4th routine
+    set_density_matrix_derivative!(du, u, sim)# 5th routine
 end
 
+# Get the current eneries, the current derivate and the current eigenvectors
+# see ../Calculators/Calculators.jl for the functions used here.
+# Then get the eigenvectors of the energy
+# then use them to transform the derivatives into the adiabatic form.
 function update_electronics!(sim::Simulation{<:IESH}, u::SurfaceHoppingPhasespace)
     Calculators.evaluate_potential!(sim.calculator, get_positions(u))
     Calculators.evaluate_derivative!(sim.calculator, get_positions(u))
@@ -58,6 +65,9 @@ function update_electronics!(sim::Simulation{<:IESH}, u::SurfaceHoppingPhasespac
     evaluate_nonadiabatic_coupling!(sim)
 end
 
+#These routines point to the previous routine and hands down an empty array,
+# the adiabatic_derivatives calculated in transform_drivatives and the 
+# energy eigenvectors
 function evaluate_nonadiabatic_coupling!(sim::Simulation{<:IESH})
     evaluate_nonadiabatic_coupling!.(
         sim.method.nonadiabatic_coupling,
@@ -65,6 +75,7 @@ function evaluate_nonadiabatic_coupling!(sim::Simulation{<:IESH})
         Ref(sim.calculator.eigenvalues))
 end
 
+# Calculates the nonadiabatic coupling. This is probably also true for IESH
 function evaluate_nonadiabatic_coupling!(coupling::Matrix, adiabatic_derivative::Matrix, eigenvalues::Vector)
     for i=1:length(eigenvalues)
         for j=i+1:length(eigenvalues)
@@ -74,6 +85,8 @@ function evaluate_nonadiabatic_coupling!(coupling::Matrix, adiabatic_derivative:
     end
 end
 
+# gets the momenta corresponding to the current state from the adiabatic derivates 
+# (transformation of the diabatic ones, see 3 routines above)
 function set_force!(du::SurfaceHoppingPhasespace, u::SurfaceHoppingPhasespace, sim::Simulation{<:IESH})
     for i=1:length(sim.atoms)
         for j=1:sim.DoFs
@@ -82,12 +95,19 @@ function set_force!(du::SurfaceHoppingPhasespace, u::SurfaceHoppingPhasespace, s
     end
 end
 
+# Calculate the _time_-derivate  of the  density matrix
+# Goes back to motion!
 function set_density_matrix_derivative!(du::SurfaceHoppingPhasespace, u::SurfaceHoppingPhasespace, sim::Simulation{<:IESH})
     σ = get_density_matrix(u)
     velocity = get_positions(du)
     V = sim.method.density_propagator
 
-    V .= diagm(sim.calculator.eigenvalues)
+    V .= diagm(sim.calculator.eigenvalues)# Creates a diagonal matrix from eigenvalues
+    # Calculation going on here from: Martens_JPhysChemA_123_1110_2019, eq. 6
+    # d is the nonadiabatic coupling matrix
+    #i ħ dσ/dt = iħ sum_l [(V_{m,l} - i ħ velocity d_{m,l})*σ_{l,n} - &
+    #                      σ_{m,l}*(V_{l,n} - iħ velocity d_{l,n})]
+    # vgl. also SubotnikBellonzi_AnnuRevPhyschem_67_387_2016, eq. 5 and 6
     for i=1:length(sim.atoms)
         for j=1:sim.DoFs
             V .-= im*velocity[j,i].*sim.method.nonadiabatic_coupling[j,i]
@@ -96,6 +116,7 @@ function set_density_matrix_derivative!(du::SurfaceHoppingPhasespace, u::Surface
     get_density_matrix(du) .= -im*(V*σ - σ*V)
 end
 
+# This sets when the condition will be true. In this case, always.
 condition(u, t, integrator::DiffEqBase.DEIntegrator) = true
 
 function affect!(integrator::DiffEqBase.DEIntegrator)
@@ -124,10 +145,12 @@ function update_hopping_probability!(integrator::DiffEqBase.DEIntegrator)
             for i=1:length(sim.atoms)
                 for j=1:sim.DoFs
                     sim.method.hopping_probability[m] += 2velocity[j,i]*real(σ[m,s]/σ[s,s])*coupling[j,i][s,m] * dt
+                    println(velocity[j,i]," ", real(σ[m,s]/σ[s,s])," ",coupling[j,i][s,m]," ", dt)
                 end
             end
         end
     end
+    println(clamp!(sim.method.hopping_probability, 0, 1))
     clamp!(sim.method.hopping_probability, 0, 1) # Restrict probabilities between 0 and 1
     cumsum!(sim.method.hopping_probability, sim.method.hopping_probability)
 end
@@ -181,4 +204,4 @@ function execute_hop!(integrator::DiffEqBase.DEIntegrator, new_state::Integer)
     integrator.u.state = new_state
 end
 
-iesh_callback = DiscreteCallback(condition, affect!; save_positions=(false, false))
+IESH_callback = DiscreteCallback(condition, affect!; save_positions=(false, false))
