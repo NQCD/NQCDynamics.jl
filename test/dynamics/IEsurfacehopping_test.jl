7dcbd090b4f4e3f36e5f65ad9dcfe31131b7ac2d
smjanke <4113052+smjanke@users.noreply.github.com>
2021-01-27 22:08:40 +0000

    Investigated hopping probability

--- test/dynamics/IEsurfacehopping_test.jl
+++ test/dynamics/IEsurfacehopping_test.jl
@@ -13,9 +13,9 @@ using Plots
 #   4) calculate switching probability, calculate if hop should occur
 #   5) If no hop, GOTO 2; if hop, see if enough energy to do it, GOTO 2
 
-n_states = 1# # number of electronic n_states
-x = 0.0 # position of 1D particle
-vinit = -0.1 # initial velocity
+n_states = 0# # number of electronic n_states
+x = 5.0 # position of 1D particle
+vinit = -0.005 # initial velocity
 step = 0.01 # MD step
 mass = 1.5 # atomic mass#
 B_na = 1.0 # parameter that defines nonadiabatic coupling strength
@@ -37,15 +37,20 @@ atoms = Atoms{Float64}([:H])
 #1c define the dynamics that will be used
 # defined in main/Dynamics/iesh.jl
 #IESH{T}(DoFs::Integer, atoms::Integer, states::Integer)
+#dynam = Dynamics.IESH{Float64}(1, 1, n_states+2,step)
 dynam = Dynamics.IESH{Float64}(1, 1, n_states+2)
 
 # Initialize the simulation problem
 # Simulation is defined in main/simulations.jl
-sim = Simulation(atoms, model, dynam; DoFs=1)
+#sim = Simulation(atoms, Models.TullyModelOne(), Dynamics.IESH{Float64}(1, 1, 2); DoFs=1)
+sim = Simulation(atoms, Models.TullyModelOne(), dynam; DoFs=1)
 
 #calculate momentum
 r = fill(x, sim.DoFs, length(sim.atoms)) 
 p = fill(vinit*atoms.masses[1], sim.DoFs, length(sim.atoms))
+
+#r = fill(-5.0, sim.DoFs, length(sim.atoms)) 
+#p = fill(8.9, sim.DoFs, length(sim.atoms)) 
 # intial state
 #k = rand(1:n_states+2)
 k = 1
@@ -53,12 +58,19 @@ k = 1
 # postions, momenta, density matrix, state
 # ../../Dynamics/iesh.jl
 z = SurfaceHoppingPhasespace(r,p, n_states+2, k)
+#display(k)
+#z = SurfaceHoppingPhasespace(R, P, 2, 1)
+
+solution = Dynamics.run_trajectory(z, (0.0, 2500.0), sim)
 
 #../../Dynamics/iesh.jl
 # Solution of Differentiall equations and propagation
-#solution = Dynamics.run_trajectory(z, (0.0, 2500), sim)
+#solution = Dynamics.run_trajectory(z, (0.0, 1000), sim, dt=step)
+#solution = Dynamics.run_trajectory(z, (0.0, 1000), sim)
 
-#a=plot(solution.t, [real(Dynamics.get_density_matrix(u)[1,1]) for u in solution.u], label="σ[1,1]")
-#plot!(solution.t, [real(Dynamics.get_density_matrix(u)[2,2]) for u in solution.u], label="σ[2,2]")
-#plot!(solution.t, [u.state for u in solution.u].-1, label="current surface")
+#a =plot(solution.t, [real(Dynamics.get_positions(u)[1,1]) for u in solution.u], label="r")
+plot(solution.t, [real(Dynamics.get_density_matrix(u)[1,1]) for u in solution.u], label="σ[1,1]", marker =2)
+plot!(solution.t, [real(Dynamics.get_density_matrix(u)[2,2]) for u in solution.u], label="σ[2,2]", marker =2)
+plot!(solution.t, [u.state for u in solution.u].-1, label="current surface")
 #display(plot(a))
+#display(plot(b))
