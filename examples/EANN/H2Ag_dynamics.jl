0eea4d5aaf72b0c16f1e6309c2cf9805133e091e
James Gardner <james.gardner1421@gmail.com>
2021-01-28 18:51:14 +0000

    Fix zero temperature mdef

--- examples/EANN/H2Ag_dynamics.jl
+++ examples/EANN/H2Ag_dynamics.jl
@@ -16,20 +16,24 @@ cell, atoms, positions = read_system(input)
 data = open(readdlm, velo_path)
 vel_0[1,:] = data[1, 5:7]
 vel_0[2,:] = data[2, 5:7]
-vel_0 ./= ps
+vel_0 = austrip.(vel_0.*u"Å/ps")
 vel_0 = copy(transpose(vel_0))
-vel_0[1,:] .*= atoms.masses
-vel_0[2,:] .*= atoms.masses
+vel_0[1,:] .*= atoms.masses[1]
+vel_0[2,:] .*= atoms.masses[2]
 
 println("Initialize...")
 cb, vals = Dynamics.create_energy_saving_callback()
 model = Models.EANN_H₂Ag(model_path, atoms)
 sim = Simulation(atoms, model, Dynamics.MDEF{Float64}(length(atoms)); temperature=0u"K", cell=cell)
 z = Phasespace(positions, vel_0)
-tspan = (0.0, austrip(1u"ps"))
-dt = austrip(0.05u"fs")
+tspan = (0.0, 5.0u"ps")
 
-@time solution = Dynamics.run_trajectory(z, tspan, sim; callback=cb, adaptive=false, dt=dt)
+@time solution = Dynamics.run_trajectory(z, tspan, sim; callback=cb, dt=0.05u"fs", reltol=1e-8)
 
 write_trajectory("trajectory.xyz", cell, atoms, get_positions.(solution.u))
-plot(solution.t, vals.saveval)
+plot(solution.t, evaluate_hamiltonian.(Ref(sim), solution.u))
+ke = NonadiabaticMolecularDynamics.evaluate_kinetic_energy.(Ref(sim), get_momenta.(solution.u))
+pe = NonadiabaticMolecularDynamics.evaluate_configurational_energy.(Ref(sim), get_positions.(solution.u))
+plot(ustrip.(auconvert.(u"ps", solution.t)), ustrip.(auconvert.(u"eV", ke)), label="kinetic")
+plot!(ustrip.(auconvert.(u"ps", solution.t)), ustrip.(auconvert.(u"eV", pe)), label="potential")
+plot!(ustrip.(auconvert.(u"ps", solution.t)), ustrip.(auconvert.(u"eV", ke.+pe)), label="total")
