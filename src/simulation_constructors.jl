443c1a0e59bb285ae63172f599965ff038924b96
James Gardner <james.gardner1421@gmail.com>
2021-01-28 13:42:57 +0000

    Add convenience constructors for simulations

---
+++ src/simulation_constructors.jl
@@ -0,0 +1,21 @@
+
+Simulation(atoms::Atoms, model::Model; kwargs...) =
+    Simulation{Classical}(atoms, model; kwargs...)
+
+Simulation{Classical}(atoms::Atoms, model::Model, kwargs...) =
+    Simulation(atoms, model, Classical(); kwargs...)
+
+RingPolymerSimulation{Classical}(atoms::Atoms, model::Model, n_beads::Integer; kwargs...) =
+    RingPolymerSimulation(atoms, model, Classical(), n_beads::Integer; kwargs...)
+
+Simulation{MDEF}(atoms::Atoms{S,T}, model::Model; DoFs=3, kwargs...) where {S,T} =
+    Simulation(atoms, model, MDEF{T}(length(atoms), DoF=DoFs); DoFs=DoFs, kwargs...)
+
+RingPolymerSimulation{NRPMD}(atoms::Atoms{S,T}, model::Model, n_beads::Integer; kwargs...) where {S,T}=
+    RingPolymerSimulation(atoms, model, NRPMD{T}(model.n_states), n_beads; kwargs...)
+
+Simulation{Langevin}(atoms::Atoms{S,T}, model::Model; η=1, kwargs...) where {S,T} =
+    Simulation(atoms, model, Langevin{T}(η), kwargs...)
+
+Simulation{FSSH}(atoms::Atoms{S,T}, model::Model; DoFs=3, kwargs...) where {S,T} =
+    Simulation(atoms, model, FSSH{T}(DoFs, length(atoms), model.n_states); kwargs...)
--- test/simulations.jl
+++ test/simulations.jl
@@ -11,3 +11,13 @@ model = Models.Free()
 
 @test Simulation(atoms, model, Classical()) isa Simulation
 @test RingPolymerSimulation(atoms, model, Classical(), 10) isa RingPolymerSimulation
+
+@test Simulation(atoms, model) isa Simulation{Classical}
+@test Simulation{Classical}(atoms, model) isa Simulation{Classical}
+@test RingPolymerSimulation{Classical}(atoms, model, 10) isa RingPolymerSimulation{Classical}
+
+# @test Simulation{MDEF}(atoms, model) isa Simulation{<:MDEF}
+
+@test RingPolymerSimulation{NRPMD}(atoms, Models.DoubleWell(), 10) isa RingPolymerSimulation{<:NRPMD}
+@test Simulation{Langevin}(atoms, model) isa Simulation{<:Langevin}
+@test Simulation{FSSH}(atoms, Models.DoubleWell()) isa Simulation{<:FSSH}
