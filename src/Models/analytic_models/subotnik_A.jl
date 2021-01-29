883d61a8585067464c3a10e0627ab8e6e24ff350
smjanke <4113052+smjanke@users.noreply.github.com>
2021-01-29 19:08:39 +0000

    Testing Model A of Subotnik and Coffman

--- src/Models/analytic_models/subotnik_A.jl
+++ src/Models/analytic_models/subotnik_A.jl
@@ -13,6 +13,7 @@ export Subotnik_A
 """
 
 struct Subotnik_A <: DiabaticModel
+    n_states::UInt8
     m::Float64
     om::Float64
     g::Float64
@@ -23,18 +24,19 @@ struct Subotnik_A <: DiabaticModel
     d::Float64
 end
 
-function Subotnik_A(
-    ;m=2000.0,
-    om=0.0002,
-    g=20.6097,
-    DG=-0.0038,
-    G1=0.0001,
-    G0=0.01,
-    K= 0.1,
-    d=8.0
-)
-Subotnik_A(m, om, g, DG, G1, G0, K, d)
-end
+#function Subotnik_A(
+#    ;N=2,
+#    m=2000.0,
+#    om=0.00002,
+#    g=20.6097,
+#   DG=-0.0038,
+#    G1=0.0001,
+#    G0=0.01,
+#    K= 0.1,
+#    d=8.0
+#    )
+Subotnik_A(m=2000.0, om=0.0002, g=20.6097, DG=-0.0038, G1=0.0001, G0=0.01, K=0.1, d=8.0) = Subotnik_A(2,m, om, g, DG, G1, G0, K, d)
+#end
 
 """
 Diabatic potenial matrix
@@ -43,24 +45,28 @@ function potential!(model::Subotnik_A, V::Hermitian, R::AbstractMatrix)
     # Update d
     d = model.g/2 + model.DG/(model.m*model.om^2*model.g)
 
+
     # Higher potential well
     V_0(r) = (model.m*model.om^2*r^2)/2.0
     # Loweer energy potential well
     V_1(r) = (model.m*model.om^2*(r-model.g)^2)/2.0 + model.DG
+    println(R[1]," " , V_0(R[1]), " ", V_1(R[1]))
     # weighted metal-molecule coupling between the diabates
     G(r)=model.G1*(model.G0 + (model.K*(r - model.d)^2)/(1.0 + model.K*(r -model.d)^2))
     
+    
+    V = Hermitian(zeros(2,2))
     # Set up the Hamiltonian
     V[1,1] = V_0(R[1])
-    V[2,2] = V_0(R[1])
-    V[1,2] = G(R[1])
-    V[2,1] = G(R[1])
+    V[2,2] = V_1(R[1])
+    V.data[1,2] = G(R[1])
+    V.data[2,1] = G(R[1])
 end
 
 """
 Elementwise position derivative of the above potential
 """
-function derivative!(model::ScatteringAndersonHolstein,D::AbstractMatrix{<:Hermitian}, R::AbstractArray)
+function derivative!(model::Subotnik_A,D::AbstractMatrix{<:Hermitian}, R::AbstractArray)
     dV0dr(r) = model.m*model.om^2*r
     dV1dr(r) = model.m*model.om^2*(r-model.g)
     dGdr(r)  = model.G1((2*model.K*(r-model.d)*(1+K*(r-model.d)^2)-2*model.K^2*(r-model.d)^2)/
