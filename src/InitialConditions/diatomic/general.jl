89f2cf9997da410c86ceea3fed822d4116ef214a
James Gardner <james.gardner1421@gmail.com>
2021-03-22 12:58:34 +0000

    Add moment of inertia tensor

--- src/InitialConditions/diatomic/general.jl
+++ src/InitialConditions/diatomic/general.jl
@@ -1,6 +1,5 @@
 using LinearAlgebra
 using FastGaussQuadrature
-using Statistics
 using LsqFit
 
 """
@@ -41,10 +40,24 @@ end
 
 centre_of_mass(x, m) = (x[:,1].*m[1] .+ x[:,2].*m[2]) ./ (m[1]+m[2])
 reduced_mass(atoms::Atoms) = reduced_mass(atoms.masses)
-reduced_mass(m) = m[1]*m[2]/(m[1]+m[2])
+reduced_mass(m::AbstractVector) = m[1]*m[2]/(m[1]+m[2])
 bond_length(r) = norm(r[:,1] .- r[:,2])
 total_angular_momentum(r, p) = norm(cross(r[:,1], p[:,1]) + cross(r[:,2], p[:,2]))
 total_moment_of_inertia(r, m) = norm(r[:,1])*m[1] + norm(r[:,2])*m[2]
+
+function moment_of_inertia_tensor(r, m)
+    tensor = zeros(3,3)
+    for atom in axes(r, 2)
+        absr = norm(r[:,atom])^2
+        tensor += m[atom] * absr * I
+        for i in axes(r, 1)
+            for j in axes(r, 1)
+                tensor[i,j] -= m[atom] * r[i,atom]*r[j,atom]
+            end
+        end
+    end
+    tensor
+end
 
 """
 Evaluate energy for different bond lengths and identify force constant. 
