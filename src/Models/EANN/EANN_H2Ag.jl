73246eba3831711c1b1e571420d984f34f350623
James Gardner <msuhmw@colas.chem.warwick.ac.uk>
2021-03-30 14:39:32 +0100

    Hopefully fix H2Ag model
    Can now specify two different paths for friction and potential.
    Works on Linux and hopefully mac too

--- src/Models/EANN/EANN_H2Ag.jl
+++ src/Models/EANN/EANN_H2Ag.jl
@@ -1,23 +1,37 @@
 export EANN_H₂Ag
 using PeriodicTable
+using Libdl
 
 """
 J. Phys. Chem. Lett. 2019, 10, 4962−4967
 J. Phys. Chem. C 2020, 124, 186−195
 """
 struct EANN_H₂Ag{S,T} <: FrictionModel
-    path::String
+    potential_function::Ptr{Nothing}
+    force_function::Ptr{Nothing}
+    friction_function::Ptr{Nothing}
+    potential_path::String
+    friction_path::String
     atoms::Atoms{S,T}
     h2indices::Vector{UInt}
     tmp_coordinates::Matrix{T}
     tmp_friction_coordinates::Matrix{T}
     tmp_friction::Matrix{T}
     tmp_energy::Matrix{T}
-    function EANN_H₂Ag(path::String, atoms::Atoms{S,T}) where {S,T}
-        initialize_H2Ag_pes(path)
+    function EANN_H₂Ag(potential_path::String, friction_path::String, atoms::Atoms{S,T}) where {S,T}
+        pes_lib = dlopen(potential_path)
+        friction_lib = dlopen(friction_path)
+
+        initialize!(potential_path, pes_lib)
+
+        potential_function = dlsym(pes_lib, "pot0_")
+        force_function = dlsym(pes_lib, "dpeshon_")
+        friction_function = dlsym(friction_lib, "tensor_")
+    
         h2indices = findall(atoms.types .== :H)
         @assert h2indices == [1, 2]
-        new{S,T}(path, atoms, h2indices,
+        new{S,T}(potential_function, force_function, friction_function,
+                 potential_path, friction_path, atoms, h2indices,
                  zeros(3, length(atoms)),
                  zeros(3, 2),
                  zeros(6, 6),
@@ -25,58 +39,39 @@ struct EANN_H₂Ag{S,T} <: FrictionModel
     end
 end
 
+function initialize!(path, pes_lib)
+    cd(splitdir(path)[1]) do
+        # EANN PES: call init_pes function
+        pes_init = dlsym(pes_lib, "pes_init_")
+        ccall(pes_init, Cvoid, ())
+    end
+end
+
 function potential!(model::EANN_H₂Ag, V::AbstractVector, R::AbstractMatrix)
     model.tmp_coordinates .= au_to_ang.(R)
-    cd(model.path) do
-        calculate_H2Ag_pes_potential!(length(model.atoms), model.tmp_coordinates, model.tmp_energy)
+    cd(splitdir(model.potential_path)[1]) do
+        ccall(model.potential_function, Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
+              length(model.atoms), model.tmp_coordinates, model.tmp_energy)
     end
     V[1] = eV_to_au(model.tmp_energy[1])
 end
 
 function derivative!(model::EANN_H₂Ag, D::AbstractMatrix, R::AbstractMatrix)
     model.tmp_coordinates .= au_to_ang.(R)
-    cd(model.path) do
-        calculate_H2Ag_pes_forces!(length(model.atoms), model.tmp_coordinates, D)
+    cd(splitdir(model.potential_path)[1]) do
+        ccall(model.force_function, Cvoid, (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
+              length(model.atoms), model.tmp_coordinates, D)
     end
     D .= eV_per_ang_to_au.(D)
 end
 
 function friction!(model::EANN_H₂Ag, F::AbstractMatrix, R::AbstractMatrix)
     @views model.tmp_friction_coordinates .= au_to_ang.(R[:,model.h2indices])
-    cd(model.path) do
-        calculate_H2Ag_friction_tensor!(model.tmp_friction_coordinates, model.tmp_friction)
+    cd(splitdir(model.friction_path)[1]) do
+        ccall(model.friction_function, Cvoid, (Ref{Float64}, Ptr{Float64}),
+              model.tmp_coordinates, model.tmp_friction)
     end
     F[1:6,1:6] .= ps_inv_to_au.(model.tmp_friction)
     F .*= austrip(elements[:H].atomic_mass)
 end
 
-function initialize_H2Ag_pes(lib_path::String)
-    cd(lib_path) do
-        # EANN PES: call init_pes function
-        ccall((:pes_init_, "h2ag111_pes.dylib"), Cvoid, ())
-    end
-end
-
-function calculate_H2Ag_pes_potential!(n_atoms::Int64, coordinates::Matrix{Float64}, energy::Matrix{Float64})
-    # EANN PES: get energy from EANN to vars: energy and force
-    ccall((:pot0_, "h2ag111_pes.dylib"),
-        Cvoid,
-        (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
-        n_atoms, coordinates, energy)
-end
-
-function calculate_H2Ag_pes_forces!(n_atoms::Int64, coordinates::Matrix{Float64}, force::Matrix{Float64})
-    # EANN PES: get force from EANN to vars: energy and force
-    ccall((:dpeshon_, "h2ag111_pes.dylib"),
-        Cvoid,
-        (Ref{Int64}, Ref{Float64}, Ptr{Float64}),
-        n_atoms, coordinates, force)
-end
-
-function calculate_H2Ag_friction_tensor!(coordinates::AbstractMatrix, friction::AbstractMatrix)
-    # EANN PES: calculate friction tensor
-    ccall((:tensor_, "h2ag111_pes_friction.dylib"),
-        Cvoid,
-        (Ref{Float64}, Ptr{Float64}),
-        coordinates, friction)
-end
