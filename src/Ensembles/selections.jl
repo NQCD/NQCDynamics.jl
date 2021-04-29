79d535b686c01c6caffce9e82867c455c6c12397
James Gardner <james.gardner1421@gmail.com>
2021-04-29 18:03:33 +0100

    Combine new and old ensemble
    Still needs some work but this will do for now.

--- src/Ensembles/selections.jl
+++ src/Ensembles/selections.jl
@@ -1,3 +1,4 @@
+using DiffEqBase: CallbackSet
 
 abstract type AbstractSelection end
 
@@ -22,12 +23,38 @@ struct RandomSelection{D} <: AbstractSelection
 end
 
 function (select::OrderedSelection)(prob, i, repeat)
-    u0 = Dynamics.select_u0(prob.p, select.distribution, i)
+    v, r = InitialConditions.pick(select.distribution, i)
+    u0 = select_u0(prob.p, v, r)
     remake(prob, u0=u0)
 end
 
 function (select::RandomSelection)(prob, i, repeat)
-    i = rand(1:length(select.distribution))
-    u0 = Dynamics.select_u0(prob.p, select.distribution, i)
+    v, r = rand(select.distribution)
+    u0 = select_u0(prob.p, v, r)
     remake(prob, u0=u0)
+end
+
+struct SelectWithCallbacks{S<:AbstractSelection,C1,C2,V} <: AbstractSelection
+    selection::S
+    standard_callbacks::C1
+    changing_callbacks::C2
+    values::V
+    function SelectWithCallbacks(selection, standard_callbacks, output, trajectories)
+
+        callbacks = []
+        values = []
+        for i=1:trajectories
+            cb, vals = Dynamics.create_saving_callback(output)
+            push!(callbacks, cb)
+            push!(values, vals)
+        end
+
+        new{typeof(selection), typeof(standard_callbacks), typeof(callbacks), typeof(values)}(
+            selection, standard_callbacks, callbacks, values)
+    end
+end
+
+function (select::SelectWithCallbacks)(prob, i, repeat)
+    prob = select.selection(prob, i, repeat)
+    remake(prob, callback=CallbackSet(select.standard_callbacks, select.changing_callbacks[i]))
 end
