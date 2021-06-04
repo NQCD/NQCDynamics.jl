82af94964c8c438c8c6c8d9abf0c1405fc57b93e
James Gardner <james.gardner1421@gmail.com>
2021-06-04 22:42:15 +0100

    Shift cells, atoms and conversions into base package
    I didn't really want to do this but realised the models might depend on these things too.
    Perhaps it would've been best to keep the models in the base package too but I like the idea of having a separate package for the models.

--- src/NonadiabaticMolecularDynamics.jl
+++ src/NonadiabaticMolecularDynamics.jl
@@ -3,14 +3,12 @@ module NonadiabaticMolecularDynamics
 using Reexport
 using Requires
 using DocStringExtensions
+
+@reexport using NonadiabaticDynamicsBase
 @reexport using NonadiabaticModels
 
-include("unit_conversions.jl")
-
-include("atoms.jl")
 include("ring_polymer_array.jl")
 include("ring_polymer.jl")
-include("cells.jl")
 include("dynamical_variables.jl")
 
 include("Calculators/Calculators.jl")
