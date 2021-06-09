2116e04600f4a48f1f84592297d95f6e4ba9626d
James Gardner <james.gardner1421@gmail.com>
2021-06-09 10:46:37 +0100

    Fix example

--- examples/dynamics/tully1_prob.jl
+++ examples/dynamics/tully1_prob.jl
@@ -4,58 +4,57 @@ using Unitful
 using UnitfulAtomic
 using Distributions
 using LinearAlgebra
+using ProgressMeter
 
-model = Models.TullyModelOne2()
-atoms = Atoms([:H])
-r = Normal(-10)
-output_scattering_fssh = zeros(2, 2, 51)
-output_scattering_ehrenfest = zeros(2, 2, 51)
-ix = 0
-mass = 2000
+model = TullyModelOne()
+atoms = Atoms(2000)
+r = Normal(-4)
+reduction = Ensembles.MeanReduction()
 
-for k in 5:0.1:10
-    print(k)
-    global ix += 1
+n_traj = 2000
+momenta = 3:0.5:30
+
+output_scattering_fssh = zeros(2, 2, length(momenta))
+output_scattering_ehrenfest = zeros(2, 2, length(momenta))
+
+@showprogress 1 "Running ensembles..." for (i, k) in enumerate(momenta)
+    v = k / atoms.masses[1]
+    distribution = InitialConditions.DynamicalDistribution(v, r, (1,1); state=1, type=:diabatic)
+    selection = Ensembles.RandomSelection(distribution)
+    distance = 10
+    time = distance / v
+    tspan = (0.0, time)
+
     sim_fssh = Simulation{FSSH}(atoms, model; DoFs=1)
-    output_fssh = Ensembles.OutputScatteringFSSH()
-    v = k/mass#sim_fssh.atoms.masses[1]
-    distribution_fssh = InitialConditions.DynamicalDistribution(v, r, (1,1); state=1, type=:diabatic)
-    selection_fssh = Ensembles.RandomSelection(distribution_fssh)
-    reduction_fssh = Ensembles.MeanReduction()
-    solution_fssh = Ensembles.run_ensemble(sim_fssh, (0.0, 3000.0), selection_fssh; trajectories=1e3,
-        output=output_fssh, reduction=reduction_fssh, saveat=10.0,dtmax=5)
-    print(" ")
-    output_scattering_fssh[:,:,ix] = solution_fssh
+    output = Ensembles.OutputStateResolvedScattering1D(sim_fssh)
+    solution_fssh = Ensembles.run_ensemble(sim_fssh, tspan, selection;
+        trajectories=n_traj, output=output, reduction=reduction)
+    output_scattering_fssh[:,:,i] .= solution_fssh.u
  
     sim_ehrenfest = Simulation{Ehrenfest}(atoms, model; DoFs=1)
-    output_ehrenfest = Ensembles.OutputScatteringEhrenfest()
-    distribution_ehrenfest = InitialConditions.DynamicalDistribution(v, r, (1,1); state=1, type=:diabatic)
-    selection_ehrenfest = Ensembles.RandomSelection(distribution_ehrenfest)
-    reduction_ehrenfest = Ensembles.MeanReduction()
-    solution_ehrenfest = Ensembles.run_ensemble(sim_ehrenfest, (0.0, 3000.0), selection_ehrenfest; trajectories=1e3,
-        output=output_ehrenfest, reduction=reduction_ehrenfest, saveat=10.0,dtmax=5)
-    output_scattering_ehrenfest[:,:,ix] = solution_ehrenfest
+    output = Ensembles.OutputStateResolvedScattering1D(sim_ehrenfest)
+    solution_ehrenfest = Ensembles.run_ensemble(sim_ehrenfest, tspan, selection;
+        trajectories=n_traj, output=output, reduction=reduction)
+    output_scattering_ehrenfest[:,:,i] .= solution_ehrenfest.u
 end
 
-rng=5:0.1:10
-
-plot_transmition1 = plot(rng, output_scattering_ehrenfest[1,2,:], title="Transmission 1", label="Ehrenfest", legend=:topleft)
-plot!(rng, output_scattering_fssh[1,2,:], label="FSSH")
+plot_transmition1 = plot(momenta, output_scattering_ehrenfest[1,2,:], title="Transmission 1", label="Ehrenfest", legend=:topleft)
+plot!(momenta, output_scattering_fssh[1,2,:], label="FSSH")
 ylabel!("Probability")
 xlabel!("k / a.u.")
 
-plot_scattering1 = plot(rng, output_scattering_ehrenfest[1,1,:], title="Scattering 1", label="Ehrenfest", legend=:bottomleft)
-plot!(rng, output_scattering_fssh[1,1,:], label="FSSH")
+plot_scattering1 = plot(momenta, output_scattering_ehrenfest[1,1,:], title="Scattering 1", label="Ehrenfest", legend=:bottomleft)
+plot!(momenta, output_scattering_fssh[1,1,:], label="FSSH")
 ylabel!("Probability")
 xlabel!("k / a.u.")
 
-plot_transmition2 = plot(rng, output_scattering_ehrenfest[2,2,:], title="Transmission 2", label="Ehrenfest", legend=:topleft)
-plot!(rng, output_scattering_fssh[2,2,:], label="FSSH")
+plot_transmition2 = plot(momenta, output_scattering_ehrenfest[2,2,:], title="Transmission 2", label="Ehrenfest", legend=:topleft)
+plot!(momenta, output_scattering_fssh[2,2,:], label="FSSH")
 ylabel!("Probability")
 xlabel!("k / a.u.")
 
-plot_scattering2 = plot(rng, output_scattering_ehrenfest[2,1,:], title="Scattering 2", label="Ehrenfest", legend=:topleft)
-plot!(rng, output_scattering_fssh[2,1,:], label="FSSH")
+plot_scattering2 = plot(momenta, output_scattering_ehrenfest[2,1,:], title="Scattering 2", label="Ehrenfest", legend=:topleft)
+plot!(momenta, output_scattering_fssh[2,1,:], label="FSSH")
 ylabel!("Probability")
 xlabel!("k / a.u.")
 
