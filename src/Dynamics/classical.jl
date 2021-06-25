89b180148003a2252e89da89e42fc4fb88b28d2d
James Gardner <james.gardner1421@gmail.com>
2021-06-25 14:25:17 +0100

    Improve performance of `apply_interbead_coupling!`

--- src/Dynamics/classical.jl
+++ src/Dynamics/classical.jl
@@ -58,11 +58,16 @@ Applies the force that arises from the harmonic springs between adjacent beads.
 Only applies the force for atoms labelled as quantum within the `RingPolymerParameters`.
 """
 function apply_interbead_coupling!(dr::AbstractArray{T,3}, r::AbstractArray{T,3}, sim::RingPolymerSimulation) where {T}
-    for i in sim.beads.quantum_atoms
-        for j=1:sim.DoFs
-            dr[j,i,:] .-= 2sim.beads.springs*r[j,i,:]
+    for i in axes(dr, 3)
+        iplus = mod1(i+1, sim.beads.n_beads)
+        iminus = mod1(i-1, sim.beads.n_beads)
+        for j in sim.beads.quantum_atoms
+            for k in axes(dr, 1)
+                dr[k,j,i] -= sim.beads.ω_n² * (2r[k,j,i] - r[k,j,iplus] - r[k,j,iminus])
+            end
         end
     end
+    return nothing
 end
 
 function create_problem(u0, tspan::Tuple, sim::Simulation{<:Classical})
