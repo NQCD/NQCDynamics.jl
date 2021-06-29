e37d6a4c50b28aa2e60af848c1c1ba6f7a0b37d9
James Gardner <james.gardner1421@gmail.com>
2021-06-29 14:24:51 +0100

    Rework calculators to work with StaticArrays

--- test/calculators.jl
+++ test/calculators.jl
@@ -26,6 +26,8 @@ end
     Calculators.evaluate_derivative!(calc, rand(1, 1))
     Calculators.eigen!(calc)
     Calculators.transform_derivative!(calc)
+    Calculators.evaluate_nonadiabatic_coupling!(calc)
+    Calculators.update_electronics!(calc, rand(1, 1))
     
     calc = Calculators.RingPolymerDiabaticCalculator{Float64}(model, 1, 1, 10)
     Calculators.evaluate_potential!(calc, rand(1, 1, 10))
@@ -34,6 +36,21 @@ end
     Calculators.evaluate_centroid_derivative!(calc, rand(1, 1, 10))
     Calculators.eigen!(calc)
     Calculators.transform_derivative!(calc)
+    Calculators.evaluate_nonadiabatic_coupling!(calc)
+    Calculators.update_electronics!(calc, rand(1, 1, 10))
+end
+
+@testset "LargeDiabatic" begin
+    model = NonadiabaticModels.MiaoSubotnik()
+    @test Calculators.LargeDiabaticCalculator{Float64}(model, 3, 5) isa Calculators.LargeDiabaticCalculator
+
+    calc = Calculators.LargeDiabaticCalculator{Float64}(model, 1, 1) 
+    Calculators.evaluate_potential!(calc, rand(1, 1))
+    Calculators.evaluate_derivative!(calc, rand(1, 1))
+    Calculators.eigen!(calc)
+    Calculators.transform_derivative!(calc)
+    Calculators.evaluate_nonadiabatic_coupling!(calc)
+    Calculators.update_electronics!(calc, rand(1, 1))
 end
 
 @testset "General constructors" begin
@@ -43,4 +60,6 @@ end
     model = NonadiabaticModels.Free()
     @test Calculators.Calculator(model, 1, 1) isa Calculators.AdiabaticCalculator
     @test Calculators.Calculator(model, 1, 1, 2) isa Calculators.RingPolymerAdiabaticCalculator
+    model = NonadiabaticModels.MiaoSubotnik()
+    @test Calculators.Calculator(model, 1, 1) isa Calculators.LargeDiabaticCalculator
 end
