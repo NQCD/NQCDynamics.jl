9c87a8df83da961ef7b3c0372301246e1c6ab1d6
wstark <wch.stark@gmail.com>
2021-07-06 17:24:52 +0100

    add centroid electronics ehrenfest

--- src/Dynamics/Ehrenfest/ehrenfest_rpmd.jl
+++ src/Dynamics/Ehrenfest/ehrenfest_rpmd.jl
@@ -1,3 +1,16 @@
+function motion!(du, u, sim::RingPolymerSimulation{<:AbstractEhrenfest}, t)
+    dr = get_positions(du)
+    dv = get_velocities(du)
+    dσ = get_quantum_subsystem(du)
+    r = get_positions(u)
+    v = get_velocities(u)
+    σ = get_quantum_subsystem(u)
+    velocity!(dr, v, r, sim, t)
+    Calculators.update_electronics!(sim.calculator, r)
+    Calculators.update_centroid_electronics!(sim.calculator, r)
+    acceleration!(dv, v, r, sim, t, σ)
+    set_quantum_derivative!(dσ, get_centroid(v), σ, sim)
+end
 
 function acceleration!(dv, v, r, sim::RingPolymerSimulation{<:Ehrenfest}, t, σ)
     dv .= zero(eltype(dv))
@@ -11,7 +24,7 @@ function acceleration!(dv, v, r, sim::RingPolymerSimulation{<:Ehrenfest}, t, σ)
     return nothing
 end
 
-function set_density_matrix_derivative!(dσ, v, σ, sim::RingPolymerSimulation{<:Ehrenfest})
+function set_quantum_derivative_old!(dσ, v, σ, sim::RingPolymerSimulation{<:Ehrenfest})
     V = sim.method.density_propagator
 
     V .= diagm(sum(sim.calculator.eigenvalues))
@@ -25,13 +38,42 @@ function set_density_matrix_derivative!(dσ, v, σ, sim::RingPolymerSimulation{<
     return nothing
 end
 
+function set_quantum_derivative!(dσ, v, σ, sim::RingPolymerSimulation{<:Ehrenfest})
+    V = sim.method.density_propagator
+
+    V .= diagm(sim.calculator.centroid_eigenvalues)
+    for I in eachindex(v)
+        @. V -= im * v[I] * sim.calculator.centroid_nonadiabatic_coupling[I]
+    end
+    #V ./= length(sim.beads)
+    mul!(sim.calculator.tmp_mat_complex1, V, σ)
+    mul!(sim.calculator.tmp_mat_complex2, σ, V)
+    @. dσ = -im * (sim.calculator.tmp_mat_complex1 - sim.calculator.tmp_mat_complex2)
+    return nothing
+end
+
 function get_diabatic_population(sim::RingPolymerSimulation{<:Ehrenfest}, u)
     Calculators.evaluate_centroid_potential!(sim.calculator, get_positions(u))
-    U = eigvecs(sim.calculator.potential[1])
-
-    σ = get_density_matrix(u)
+    #U = eigvecs(sim.calculator.potential[1])
+    U = eigvecs(sim.calculator.centroid_potential)
+    σ = get_quantum_subsystem(u)
     return real.(diag(U * σ * U'))
 end
+
+# function get_diabatic_population(sim::RingPolymerSimulation{<:Ehrenfest}, u)
+#     Calculators.evaluate_potential!(sim.calculator, get_positions(u))
+#     Calculators.eigen!(sim.calculator)
+#     population = zeros(sim.calculator.model.n_states)
+#     σ = get_quantum_subsystem(u)
+#     for i=1:length(sim.beads)
+#         U = sim.calculator.eigenvectors[i]
+#         population .+= real.(diag(U * σ * U'))
+#     end
+#     population ./= length(sim.beads)
+#     #U = eigvecs(sim.calculator.potential[1])
+
+#     return population #real.(diag(U * σ * U'))
+# end
 
 function NonadiabaticMolecularDynamics.evaluate_hamiltonian(sim::RingPolymerSimulation{<:Ehrenfest}, u)
     k = evaluate_kinetic_energy(sim.atoms.masses, get_velocities(u))
