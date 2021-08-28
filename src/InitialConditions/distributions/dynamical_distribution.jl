7d7fa5e032fd5f5e41691c161c83deb810de7b82
James Gardner <james.gardner1421@gmail.com>
2021-08-28 19:33:47 +0100

    Move and rename files

--- src/InitialConditions/distributions/dynamical_distribution.jl
+++ src/InitialConditions/distributions/dynamical_distribution.jl
@@ -56,27 +56,3 @@ function Base.show(io::IO, ::MIME"text/plain", s::DynamicalDistribution)
           "state: ", s.state, "\n  ",
           "type: ", s.type)
 end
-
-struct BoltzmannVelocityDistribution{T} <: Sampleable{Multivariate,Continuous}
-    dist::MvNormal{T}
-end
-
-function BoltzmannVelocityDistribution(temperature, masses)
-    dist = MvNormal(sqrt.(temperature ./ masses))
-    BoltzmannVelocityDistribution(dist)
-end
-
-Base.length(s::BoltzmannVelocityDistribution) = length(s.dist)
-function Distributions._rand!(rng::AbstractRNG, s::BoltzmannVelocityDistribution, x::AbstractVector{<:Real})
-    Distributions._rand!(rng, s.dist, x)
-end
-function select_item(x::BoltzmannVelocityDistribution, ::Integer, size::Tuple{Int,Int})
-    permutedims(rand(x, size[1]))
-end
-function select_item(x::BoltzmannVelocityDistribution, ::Integer, size::Tuple{Int,Int,Int})
-    out = zeros(eltype(x), size)
-    for i=1:size[3]
-        out[:,:,i] .= permutedims(rand(x, size[1]))
-    end
-    return out
-end
