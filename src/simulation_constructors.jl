439090b950e71a3e1767bddee885b2ea0a2157b7
James Gardner <james.gardner1421@gmail.com>
2021-09-16 14:11:40 +0100

    Expand definitions for constructors

--- src/simulation_constructors.jl
+++ src/simulation_constructors.jl
@@ -1,45 +1,57 @@
+using NonadiabaticModels: nstates
 
-Simulation(atoms::Atoms, model::Model; kwargs...) =
+using .Dynamics.ClassicalMethods: Classical, Langevin, ThermalLangevin, MDEF
+using .Dynamics.MappingVariableMethods: NRPMD
+using .Dynamics.SurfaceHoppingMethods: FSSH, IESH
+using .Dynamics.EhrenfestMethods: Ehrenfest
+
+function Simulation(atoms::Atoms, model::Model; kwargs...)
     Simulation{Classical}(atoms, model; kwargs...)
-
-RingPolymerSimulation(atoms::Atoms, model::Model, n_beads::Integer; kwargs...) =
+end
+function RingPolymerSimulation(atoms::Atoms, model::Model, n_beads::Integer; kwargs...)
     RingPolymerSimulation{Classical}(atoms, model, n_beads; kwargs...)
+end
 
-Simulation{Classical}(atoms::Atoms, model::Model; kwargs...) =
+function Simulation{Classical}(atoms::Atoms, model::Model; kwargs...)
     Simulation(atoms, model, Classical(); kwargs...)
-
-RingPolymerSimulation{Classical}(atoms::Atoms, model::Model, n_beads::Integer; kwargs...) =
+end
+function RingPolymerSimulation{Classical}(atoms::Atoms, model::Model, n_beads::Integer; kwargs...)
     RingPolymerSimulation(atoms, model, Classical(), n_beads::Integer; kwargs...)
+end
 
-Simulation{MDEF}(atoms::Atoms, model::Model; DoFs=3, kwargs...) =
+function Simulation{MDEF}(atoms::Atoms, model::Model; DoFs=3, kwargs...)
     Simulation(atoms, model, MDEF(atoms.masses, DoFs); DoFs=DoFs, kwargs...)
-
-RingPolymerSimulation{MDEF}(atoms::Atoms, model::Model, n_beads::Integer; DoFs=3, kwargs...) =
+end
+function RingPolymerSimulation{MDEF}(atoms::Atoms, model::Model, n_beads::Integer; DoFs=3, kwargs...)
     RingPolymerSimulation(atoms, model, MDEF(atoms.masses, DoFs), n_beads; DoFs=DoFs, kwargs...)
+end
     
-RingPolymerSimulation{NRPMD}(atoms::Atoms{S,T}, model::Model, n_beads::Integer; kwargs...) where {S,T}=
-    RingPolymerSimulation(atoms, model, NRPMD{T}(model.n_states), n_beads; kwargs...)
+function RingPolymerSimulation{NRPMD}(atoms::Atoms{S,T}, model::Model, n_beads::Integer; kwargs...) where {S,T}
+    RingPolymerSimulation(atoms, model, NRPMD{T}(nstates(model)), n_beads; kwargs...)
+end
 
-Simulation{Langevin}(atoms::Atoms{S,T}, model::Model; γ=1, temperature=0u"K", DoFs=3, kwargs...) where {S,T} =
+function Simulation{Langevin}(atoms::Atoms{S,T}, model::Model; γ=1, temperature=0u"K", DoFs=3, kwargs...) where {S,T}
     Simulation(atoms, model, Langevin{T}(γ, austrip(temperature), atoms.masses, DoFs);
                temperature=temperature, DoFs=DoFs, kwargs...)
-
-# FSSH is defined in src/Dynamics/SurfaceHopping
-Simulation{FSSH}(atoms::Atoms{S,T}, model::Model; kwargs...) where {S,T} =
-    Simulation(atoms, model, FSSH{T}(model.n_states); kwargs...)
-
-# IESH is defined in src/Dynamics/SurfaceHopping
-Simulation{IESH}(atoms::Atoms{S,T}, model::Model; n_electrons, kwargs...) where {S,T} =
-    Simulation(atoms, model, IESH{T}(model.n_states, n_electrons); kwargs...)
-
-RingPolymerSimulation{FSSH}(atoms::Atoms{S,T}, model::Model, n_beads::Integer; kwargs...) where {S,T} =
-    RingPolymerSimulation(atoms, model, FSSH{T}(model.n_states), n_beads; kwargs...)
-
-RingPolymerSimulation{ThermalLangevin}(atoms::Atoms, model::Model, n_beads::Integer; γ=1, kwargs...) =
+end
+function RingPolymerSimulation{ThermalLangevin}(atoms::Atoms, model::Model, n_beads::Integer; γ=1, kwargs...)
     RingPolymerSimulation(atoms, model, ThermalLangevin(γ), n_beads; kwargs...)
+end
 
-Simulation{Ehrenfest}(atoms::Atoms{S,T}, model::Model; kwargs...) where {S,T} =
-    Simulation(atoms, model, Ehrenfest{T}(model.n_states); kwargs...)
+function Simulation{FSSH}(atoms::Atoms{S,T}, model::Model; kwargs...) where {S,T}
+    Simulation(atoms, model, FSSH{T}(nstates(model)); kwargs...)
+end
+function RingPolymerSimulation{FSSH}(atoms::Atoms{S,T}, model::Model, n_beads::Integer; kwargs...) where {S,T}
+    RingPolymerSimulation(atoms, model, FSSH{T}(nstates(n_states)), n_beads; kwargs...)
+end
 
-RingPolymerSimulation{Ehrenfest}(atoms::Atoms{S,T}, model::Model, n_beads::Integer; kwargs...) where {S,T} =
-    RingPolymerSimulation(atoms, model, Ehrenfest{T}(model.n_states), n_beads; kwargs...)
+function Simulation{IESH}(atoms::Atoms{S,T}, model::Model; n_electrons, kwargs...) where {S,T}
+    Simulation(atoms, model, IESH{T}(nstates(n_states), n_electrons); kwargs...)
+end
+
+function Simulation{Ehrenfest}(atoms::Atoms{S,T}, model::Model; kwargs...) where {S,T}
+    Simulation(atoms, model, Ehrenfest{T}(nstates(n_states)); kwargs...)
+end
+function RingPolymerSimulation{Ehrenfest}(atoms::Atoms{S,T}, model::Model, n_beads::Integer; kwargs...) where {S,T}
+    RingPolymerSimulation(atoms, model, Ehrenfest{T}(nstates(model)), n_beads; kwargs...)
+end
