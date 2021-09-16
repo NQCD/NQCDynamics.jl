121c54cc4aa9f05c0df7a4f225bd36b75ff5d305
James Gardner <james.gardner1421@gmail.com>
2021-09-16 13:22:44 +0100

    Move dynamics variables into Dynamics.jl

--- src/Dynamics/Dynamics.jl
+++ src/Dynamics/Dynamics.jl
@@ -23,6 +23,12 @@ using ..NonadiabaticMolecularDynamics: AbstractSimulation, Simulation, RingPolym
 using DiffEqBase: DiffEqBase
 using TypedTables: TypedTables
 using UnitfulAtomic: austrip
+using RecursiveArrayTools: ArrayPartition
+using ComponentArrays: ComponentVector
+
+export DynamicsVariables
+export get_positions
+export get_velocities
 
 """
 Each type of dynamics subtypes `Method` which is passed to
@@ -79,7 +85,17 @@ get_callbacks(::AbstractSimulation) = nothing
 
 function set_quantum_derivative! end
 
-include("dynamics_variables.jl")
+DynamicsVariables(::AbstractSimulation, v, r) = ComponentVector(v=v, r=r)
+get_positions(u::ComponentVector) = u.r
+get_velocities(u::ComponentVector) = u.v
+get_flat_positions(u::ComponentVector) = @view get_positions(u)[:]
+get_flat_velocities(u::ComponentVector) = @view get_velocities(u)[:]
+
+# These must be kept since the symplectic integrators (VelocityVerlet) in DiffEq use ArrayPartitions.
+get_positions(u::ArrayPartition) = u.x[2]
+get_velocities(u::ArrayPartition) = u.x[1]
+
+function get_quantum_subsystem end
 
 include("ClassicalMethods/ClassicalMethods.jl")
 
