a2ffc9ad363ce07560b4ebf3ddfc6bc5e65a1f27
James Gardner <james.gardner1421@gmail.com>
2021-10-13 15:15:55 +0100

    Fix broken link

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter
 using DocumenterCitations
 using NonadiabaticMolecularDynamics
 using NonadiabaticModels
-using CubeLDFAModel
+using CubeLDFAModel, NNInterfaces
 
 DocMeta.setdocmeta!(NonadiabaticMolecularDynamics, :DocTestSetup, :(using NonadiabaticMolecularDynamics); recursive=true)
 DocMeta.setdocmeta!(NonadiabaticModels, :DocTestSetup, :(using NonadiabaticModels, Symbolics); recursive=true)
