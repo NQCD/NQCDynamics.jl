d35e23ea6636e985a69b59e779acc1ddb756cf6b
James Gardner <james.gardner1421@gmail.com>
2021-10-14 15:28:58 +0100

    Fix more compatibility issues

--- src/DynamicsMethods/MappingVariableMethods/cmm.jl
+++ src/DynamicsMethods/MappingVariableMethods/cmm.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra: lmul!
 
 struct CMM2{T} <: DynamicsMethods.Method
     temp_q::Vector{T}
@@ -11,10 +12,10 @@ function NonadiabaticMolecularDynamics.Simulation{CMM2}(atoms::Atoms{S,T}, model
     NonadiabaticMolecularDynamics.Simulation(atoms, model, CMM2{T}(NonadiabaticModels.nstates(model)); kwargs...)
 end
 
-function DynamicsMethods.DynamicsVariables(sim::Simulation{<:CMM2}, v, r, states::Integer; type=:diabatic)
+function DynamicsMethods.DynamicsVariables(sim::Simulation{<:CMM2}, v, r, state::Integer; type=:diabatic)
 
-    qmap = zeros(n_states)
-    pmap = zeros(n_states)
+    qmap = zeros(NonadiabaticModels.nstates(sim))
+    pmap = zeros(NonadiabaticModels.nstates(sim))
 
     θ = rand() * 2π
     qmap[state] = cos(θ) * sqrt(2)
@@ -23,7 +24,7 @@ function DynamicsMethods.DynamicsVariables(sim::Simulation{<:CMM2}, v, r, states
     ComponentVector(v=v, r=r, pmap=pmap, qmap=qmap)
 end
 
-function motion!(du, u, sim::Simulation{<:CMM2}, t)
+function DynamicsMethods.motion!(du, u, sim::Simulation{<:CMM2}, t)
     dr = DynamicsUtils.get_positions(du)
     dv = DynamicsUtils.get_velocities(du)
     r = DynamicsUtils.get_positions(u)
@@ -36,7 +37,7 @@ end
 
 function acceleration!(dv, u, sim::Simulation{<:CMM2})
 
-    Calculators.evaluate_derivative!(sim.calculator, get_positions(u))
+    Calculators.evaluate_derivative!(sim.calculator, DynamicsUtils.get_positions(u))
     qmap = get_mapping_positions(u)
 	pmap = get_mapping_momenta(u)
     for I in eachindex(dv)
@@ -47,11 +48,11 @@ function acceleration!(dv, u, sim::Simulation{<:CMM2})
         dv[I] += dot(pmap, sim.method.temp_p)
     end
     lmul!(-1/2, dv)
-    divide_by_mass!(dv, sim.atoms.masses)
+    DynamicsUtils.divide_by_mass!(dv, sim.atoms.masses)
 end
 
 function set_mapping_force!(du, u, sim::Simulation{<:CMM2})
-    Calculators.evaluate_potential!(sim.calculator, get_positions(u))
+    Calculators.evaluate_potential!(sim.calculator, DynamicsUtils.get_positions(u))
     V = sim.calculator.potential
     mul!(get_mapping_positions(du), V, get_mapping_momenta(u))
     mul!(get_mapping_momenta(du), V, get_mapping_positions(u))
@@ -64,11 +65,11 @@ function get_diabatic_population(::Simulation{<:CMM2}, u)
     (qmap.^2 .+ pmap.^2) / 2
 end
 
-function DyanmicsUtils.classical_hamiltonian(sim::Simulation{<:CMM2}, u)
+function DynamicsUtils.classical_hamiltonian(sim::Simulation{<:CMM2}, u)
     r = DynamicsUtils.get_positions(u)
     v = DynamicsUtils.get_velocities(u)
 
-    kinetic = DynamicsUtils.classical_kinetic_energy(sim.atoms.masses, v)
+    kinetic = DynamicsUtils.classical_kinetic_energy(sim, v)
 
     Calculators.evaluate_potential!(sim.calculator, r)
     V = sim.calculator.potential
--- test/dynamics/cmm.jl
+++ test/dynamics/cmm.jl
@@ -3,27 +3,27 @@ using Test
 using NonadiabaticMolecularDynamics
 using FiniteDiff
 
-function test_motion!(sim::Simulation{<:CMM2}, u::Dynamics.MeyerMillerMappingVariables)
-    f(x) = evaluate_hamiltonian(sim, x)
+function test_motion!(sim::Simulation{<:CMM2}, u)
+    f(x) = DynamicsUtils.classical_hamiltonian(sim, x)
 
     grad = FiniteDiff.finite_difference_gradient(f, u)
 
     du = zero(u)
-    Dynamics.motion!(du, u, sim, 0.0)
+    DynamicsMethods.motion!(du, u, sim, 0.0)
 
-    @test get_positions(du) ≈ get_velocities(grad) ./ sim.atoms.masses' rtol=1e-3
-    @test get_velocities(du) ≈ -get_positions(grad) ./ sim.atoms.masses' rtol=1e-3
-    @test Dynamics.get_mapping_positions(du) ≈ Dynamics.get_mapping_momenta(grad) rtol=1e-3
-    @test Dynamics.get_mapping_momenta(du) ≈ -Dynamics.get_mapping_positions(grad) rtol=1e-3
+    @test DynamicsUtils.get_positions(du) ≈ DynamicsUtils.get_velocities(grad) ./ sim.atoms.masses' rtol=1e-3
+    @test DynamicsUtils.get_velocities(du) ≈ -DynamicsUtils.get_positions(grad) ./ sim.atoms.masses' rtol=1e-3
+    @test DynamicsMethods.MappingVariableMethods.get_mapping_positions(du) ≈ DynamicsMethods.MappingVariableMethods.get_mapping_momenta(grad) rtol=1e-3
+    @test DynamicsMethods.MappingVariableMethods.get_mapping_momenta(du) ≈ -DynamicsMethods.MappingVariableMethods.get_mapping_positions(grad) rtol=1e-3
 end
 
-sim = Simulation{CMM2}(Atoms(1), DoubleWell(); DoFs=1)
+sim = Simulation{CMM2}(Atoms(1), DoubleWell())
 
 v = randn(1,1)
 r = randn(1,1)
-u = Dynamics.MeyerMillerMappingVariables(v, r, 2, 1)
+u = DynamicsVariables(sim, v, r, 1)
 
 test_motion!(sim, u)
 
-sol = Dynamics.run_trajectory(u, (0, 100.0), sim; output=(:hamiltonian, :position), reltol=1e-6)
+sol = run_trajectory(u, (0, 100.0), sim; output=(:hamiltonian, :position), reltol=1e-10, abstol=1e-10)
 @test sol.hamiltonian[1] ≈ sol.hamiltonian[end] rtol=1e-3
