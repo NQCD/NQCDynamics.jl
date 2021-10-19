f9f4365ae0f55aa8c9e9d0131ef47f0e8009a37a
James Gardner <james.gardner1421@gmail.com>
2021-10-19 10:38:42 +0100

    Split tests into groups

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,35 @@
 using Test
 using SafeTestsets
-using Documenter
 using NonadiabaticMolecularDynamics
 
-doctestsetup = quote
-    using NonadiabaticMolecularDynamics
-    using Plots
-    using Symbolics
-end 
-DocMeta.setdocmeta!(NonadiabaticMolecularDynamics, :DocTestSetup, doctestsetup; recursive=true)
-doctest(NonadiabaticMolecularDynamics)
+const GROUP = get(ENV, "GROUP", "All")
 
-@time @safetestset "Calculator Tests" begin include("calculators.jl") end
-@time @safetestset "Simulation Tests" begin include("simulations.jl") end
-@time @safetestset "Ring Polymer Tests" begin include("ring_polymers.jl") end
-@time @safetestset "RingPolymerArrays Tests" begin include("ring_polymer_array.jl") end
-@time @safetestset "Monte Carlo Tests" begin include("monte_carlo.jl") end
-@time @safetestset "AdvancedMH Sampling Tests" begin include("advancedmh_sampling.jl") end
-include("dynamics/dynamics.jl")
-@time @safetestset "Distribution Tests" begin include("nuclear_distributions.jl") end
-@time @safetestset "Estimator tests" begin include("estimators.jl") end
-@time @safetestset "Harmonic Wigner distribution tests" begin include("harmonic_wigner.jl") end
-@time @safetestset "Ensemble Tests" begin include("ensembles.jl") end
+if GROUP == "All" || GROUP == "Core"
+    @time @safetestset "Calculator Tests" begin include("calculators.jl") end
+    @time @safetestset "Simulation Tests" begin include("simulations.jl") end
+    @time @safetestset "Ring Polymer Tests" begin include("ring_polymers.jl") end
+    @time @safetestset "RingPolymerArrays Tests" begin include("ring_polymer_array.jl") end
+end
+
+if GROUP == "All" || GROUP == "InitialConditions"
+    @time @safetestset "Monte Carlo Tests" begin include("monte_carlo.jl") end
+    @time @safetestset "AdvancedMH Sampling Tests" begin include("advancedmh_sampling.jl") end
+    @time @safetestset "Distribution Tests" begin include("nuclear_distributions.jl") end
+    @time @safetestset "Estimator tests" begin include("estimators.jl") end
+    @time @safetestset "Harmonic Wigner distribution tests" begin include("harmonic_wigner.jl") end
+end
+
+if GROUP == "All" || GROUP == "Dynamics"
+    @time @safetestset "Classical Tests" begin include("dynamics/classical.jl") end
+    @time @safetestset "Langevin Tests" begin include("dynamics/langevin.jl") end
+    @time @safetestset "MDEF BAOAB Tests" begin include("dynamics/mdef_baoab.jl") end
+    @time @safetestset "MDEF Tests" begin include("dynamics/mdef.jl") end
+    @time @safetestset "RPMDEF Tests" begin include("dynamics/rpmdef.jl") end
+    @time @safetestset "FSSH Tests" begin include("dynamics/fssh.jl") end
+    @time @safetestset "NRPMD Tests" begin include("dynamics/nrpmd.jl") end
+    @time @safetestset "CMM Tests" begin include("dynamics/cmm.jl") end
+    @time @safetestset "Cell Boundary Callback Tests" begin include("dynamics/cell_boundary_callback.jl") end
+    @time @safetestset "Ehrenfest Tests" begin include("dynamics/ehrenfest.jl") end
+    @time @safetestset "IESH Tests" begin include("dynamics/iesh.jl") end
+    @time @safetestset "Ensemble Tests" begin include("ensembles.jl") end
+end
