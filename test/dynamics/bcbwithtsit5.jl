032938bb5328909351a37156a89b61559e0f224c
James Gardner <james.gardner1421@gmail.com>
2021-10-19 15:17:45 +0100

    test: reduce step size to fix test

--- test/dynamics/bcbwithtsit5.jl
+++ test/dynamics/bcbwithtsit5.jl
@@ -6,7 +6,7 @@ using Random: seed!
 @testset "Ehrenfest" begin
     sim = RingPolymerSimulation{Ehrenfest}(Atoms(2000), TullyModelOne(), 10; temperature=1e-3)
     u = DynamicsVariables(sim, fill(10/2000, size(sim)), fill(-5, size(sim)) .+ randn(size(sim)), 1)
-    dt = 1.0
+    dt = 0.1
 
     sol = run_trajectory(u, (0, 2000.0), sim; algorithm=DynamicsMethods.IntegrationAlgorithms.BCBwithTsit5(), output=(:population), saveat=0:10:2000, dt=dt)
     sol1 = run_trajectory(u, (0, 2000.0), sim; algorithm=Tsit5(), output=(:population), saveat=0:10:2000)
@@ -17,7 +17,7 @@ end
 @testset "FSSH" begin
     sim = RingPolymerSimulation{FSSH}(Atoms(2000), TullyModelOne(), 10; temperature=1e-3)
     u = DynamicsVariables(sim, fill(10/2000, size(sim)), fill(-5, size(sim)) .+ randn(size(sim)), 1)
-    dt = 1.0
+    dt = 0.1
 
     seed!(1)
     sol = run_trajectory(u, (0, 2000.0), sim; algorithm=DynamicsMethods.IntegrationAlgorithms.BCBwithTsit5(), output=(:population), saveat=0:10:2000, dt=dt, adaptive=false)
