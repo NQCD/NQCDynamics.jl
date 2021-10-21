3ec61b55dbbc55b2c63418f206aa11a3bfb72c0f
James Gardner <james.gardner1421@gmail.com>
2021-10-21 16:37:33 +0100

    feature: add gamma parameter to CMM2 -> eCMM

--- test/dynamics/cmm.jl
+++ test/dynamics/cmm.jl
@@ -3,7 +3,7 @@ using Test
 using NonadiabaticMolecularDynamics
 using FiniteDiff
 
-function test_motion!(sim::Simulation{<:CMM2}, u)
+function test_motion!(sim::Simulation{<:eCMM}, u)
     f(x) = DynamicsUtils.classical_hamiltonian(sim, x)
 
     grad = FiniteDiff.finite_difference_gradient(f, u)
@@ -17,13 +17,21 @@ function test_motion!(sim::Simulation{<:CMM2}, u)
     @test DynamicsMethods.MappingVariableMethods.get_mapping_momenta(du) ≈ -DynamicsMethods.MappingVariableMethods.get_mapping_positions(grad) rtol=1e-3
 end
 
-sim = Simulation{CMM2}(Atoms(1), DoubleWell())
+sim = Simulation{eCMM}(Atoms(1), DoubleWell(); γ=0)
+sim1 = Simulation{eCMM}(Atoms(1), DoubleWell(); γ=0.5)
 
 v = randn(1,1)
 r = randn(1,1)
 u = DynamicsVariables(sim, v, r, 1)
+u1 = DynamicsVariables(sim1, v, r, 1)
 
 test_motion!(sim, u)
+test_motion!(sim1, u1)
 
-sol = run_trajectory(u, (0, 100.0), sim; output=(:hamiltonian, :position), reltol=1e-10, abstol=1e-10)
+sol = run_trajectory(u, (0, 100.0), sim; output=(:hamiltonian, :position, :population), reltol=1e-10, abstol=1e-10)
 @test sol.hamiltonian[1] ≈ sol.hamiltonian[end] rtol=1e-3
+@test all(isapprox.(sum.(sol.population), 1, rtol=1e-3))
+
+sol = run_trajectory(u1, (0, 100.0), sim1; output=(:hamiltonian, :position, :population), reltol=1e-10, abstol=1e-10)
+@test sol.hamiltonian[1] ≈ sol.hamiltonian[end] rtol=1e-3
+@test all(isapprox.(sum.(sol.population), 1, rtol=1e-3))
