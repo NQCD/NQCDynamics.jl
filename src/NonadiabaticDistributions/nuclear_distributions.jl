7b5fd611b25d5eca9d7307fee1be3595c42a5cec
James Gardner <james.gardner1421@gmail.com>
2021-10-25 16:40:00 +0100

    Setup new distribution structure

---
+++ src/NonadiabaticDistributions/nuclear_distributions.jl
@@ -0,0 +1,16 @@
+
+abstract type NuclearDistribution <: NonadiabaticDistribution end
+
+abstract type PositionDistribution <: NuclearDistribution end
+abstract type VelocityDistribution <: NuclearDistribution end
+
+include("dynamical_distribution.jl")
+export DynamicalDistribution
+
+include("boltzmann_velocity.jl")
+export BoltzmannVelocityDistribution
+
+include("harmonic_wigner.jl")
+export MomentumHarmonicWigner
+export PositionHarmonicWigner
+export VelocityHarmonicWigner
