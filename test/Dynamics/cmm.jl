4b2b453a50dae990f8d752680cff86949553e14a
James Gardner <38594562+jamesgardner1421@users.noreply.github.com>
2021-10-27 13:23:28 +0100

    feat: add population correlation ensemble output and update eCMM (#180)

--- test/Dynamics/cmm.jl
+++ test/Dynamics/cmm.jl
@@ -2,6 +2,7 @@
 using Test
 using NonadiabaticMolecularDynamics
 using FiniteDiff
+using LinearAlgebra: norm
 
 function test_motion!(sim::Simulation{<:eCMM}, u)
     f(x) = DynamicsUtils.classical_hamiltonian(sim, x)
@@ -17,7 +18,7 @@ function test_motion!(sim::Simulation{<:eCMM}, u)
     @test DynamicsMethods.MappingVariableMethods.get_mapping_momenta(du) ≈ -DynamicsMethods.MappingVariableMethods.get_mapping_positions(grad) rtol=1e-3
 end
 
-sim = Simulation{eCMM}(Atoms(1), DoubleWell(); γ=0)
+sim = Simulation{eCMM}(Atoms(1), DoubleWell(); γ=0.0)
 sim1 = Simulation{eCMM}(Atoms(1), DoubleWell(); γ=0.5)
 
 v = randn(1,1)
@@ -28,10 +29,41 @@ u1 = DynamicsVariables(sim1, v, r, SingleState(1))
 test_motion!(sim, u)
 test_motion!(sim1, u1)
 
-sol = run_trajectory(u, (0, 100.0), sim; output=(:hamiltonian, :position, :population), reltol=1e-10, abstol=1e-10)
+sol = run_trajectory(u, (0, 100.0), sim; output=(:hamiltonian, :position, :u), reltol=1e-10, abstol=1e-10)
 @test sol.hamiltonian[1] ≈ sol.hamiltonian[end] rtol=1e-3
-@test all(isapprox.(sum.(sol.population), 1, rtol=1e-3))
+qmap = [u.qmap for u in sol.u]
+pmap = [u.pmap for u in sol.u]
+total_population = sum.(DynamicsMethods.MappingVariableMethods.mapping_kernel.(qmap, pmap, sim.method.γ))
+@test all(isapprox.(total_population, 1, rtol=1e-3))
 
-sol = run_trajectory(u1, (0, 100.0), sim1; output=(:hamiltonian, :position, :population), reltol=1e-10, abstol=1e-10)
+sol = run_trajectory(u1, (0, 100.0), sim1; output=(:hamiltonian, :position, :u), reltol=1e-10, abstol=1e-10)
 @test sol.hamiltonian[1] ≈ sol.hamiltonian[end] rtol=1e-3
-@test all(isapprox.(sum.(sol.population), 1, rtol=1e-3))
+total_population = sum.(DynamicsMethods.MappingVariableMethods.mapping_kernel.(qmap, pmap, sim.method.γ))
+@test all(isapprox.(total_population, 1, rtol=1e-3))
+
+@testset "generate_random_points_on_nsphere" begin
+    points = DynamicsMethods.MappingVariableMethods.generate_random_points_on_nsphere(10, 1)
+    @test norm(points) ≈ 1
+    points = DynamicsMethods.MappingVariableMethods.generate_random_points_on_nsphere(10, 10)
+    @test norm(points) ≈ 10
+end
+
+@testset "Population correlation" begin
+    # Tests that the initial state correlated with itself is 1 and correlated with other state is 0.
+    gams = -0.5:0.5:1.5
+    for γ in gams[2:end]
+        @testset "Gamma = $γ" begin
+            sim = Simulation{eCMM}(Atoms(1), DoubleWell(); γ=γ)
+            out = zeros(2, 2)
+            n = 1e4
+            for i=1:n
+                u = DynamicsVariables(sim, 0, 0, SingleState(1))
+                K = DynamicsMethods.MappingVariableMethods.mapping_kernel(u.qmap, u.pmap, sim.method.γ)
+                Kinv = DynamicsMethods.MappingVariableMethods.inverse_mapping_kernel(u.qmap, u.pmap, sim.method.γ)
+                out .+= 2K * Kinv'
+            end
+            @test out ./ n ≈ [1 0; 0 1] atol=0.1
+        end
+    end
+end
+
---
+++ test/Ensembles/outputs.jl
@@ -0,0 +1,66 @@
+using NonadiabaticMolecularDynamics
+using Test
+using ComponentArrays: ComponentVector
+
+@testset "OutputFinal" begin
+    output = Ensembles.OutputFinal()
+    @test output(1:10, 1) == (10, false)
+end
+
+@testset "OutputDissociation" begin
+    output = Ensembles.OutputDissociation(1.0, (1, 2))
+    r = [1 0; 0 0; 0 0]
+    v = zeros(3, 2)
+    u = ComponentVector(v=v, r=r)
+    sol = [u]
+    @test output(sol, 1) == (0, false)
+    r = [2 0; 0 0; 0 0]
+    u = ComponentVector(v=v, r=r)
+    sol = [u]
+    @test output(sol, 1) == (1, false)
+end
+
+@testset "OutputDiabaticPopulation" begin
+    sim = Simulation{FSSH}(Atoms(1), TullyModelTwo())
+    output = Ensembles.OutputDiabaticPopulation(sim)
+end
+
+@testset "OutputQuantisedDiatomic" begin
+    sim = Simulation(Atoms(1), Harmonic())
+    output = Ensembles.OutputQuantisedDiatomic(sim; height=1, normal_vector = [1, 1, 1])
+    output = Ensembles.OutputQuantisedDiatomic(sim)
+end
+
+@testset "PopulationCorrelation" begin
+
+    @testset "Ehrenfest" begin
+        sim = Simulation{Ehrenfest}(Atoms(2000), TullyModelTwo())
+        output = Ensembles.PopulationCorrelation(sim, 1, Adiabatic())
+        u = DynamicsVariables(sim, hcat(20/2000), hcat(-5), SingleState(1, Adiabatic()))
+        sol = run_trajectory(u, (0, 1000.0), sim)
+        out, cont = output(sol, 1)
+        @test out[1][1,1] ≈ 1
+        @test out[1][2,2] ≈ 0
+        @test [o[2,1] for o in out] ≈ [1 - o[1,1] for o in out]
+    end
+
+    @testset "FSSH" begin
+        sim = Simulation{FSSH}(Atoms(2000), TullyModelTwo())
+        output = Ensembles.PopulationCorrelation(sim, 1, Adiabatic())
+        u = DynamicsVariables(sim, hcat(20/2000), hcat(-5), SingleState(1, Adiabatic()))
+        sol = run_trajectory(u, (0, 1000.0), sim)
+        out, cont = output(sol, 1)
+        @test out[1][1,1] ≈ 1
+        @test out[1][2,2] ≈ 0
+        @test [o[2,1] for o in out] ≈ [1 - o[1,1] for o in out]
+    end
+
+    @testset "eCMM" begin
+        sim = Simulation{eCMM}(Atoms(2000), TullyModelTwo())
+        output = Ensembles.PopulationCorrelation(sim, 1, Diabatic())
+        u = DynamicsVariables(sim, hcat(20/2000), hcat(-5), SingleState(1, Diabatic()))
+        sol = run_trajectory(u, (0, 1000.0), sim)
+        out, cont = output(sol, 1)
+    end
+    
+end
