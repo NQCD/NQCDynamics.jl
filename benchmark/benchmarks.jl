f9cf6b0cd09431d3ed8adf7f6e75ac65c728ebd8
James Gardner <james.gardner1421@gmail.com>
2021-11-03 16:23:46 +0000

    ci: add benchmarksk

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -15,9 +15,9 @@ tspan = (0.0, 2500.0)
 r = hcat(-5.0)
 v = hcat(8.9) ./ atoms.masses[1]
 for type in types
-    sim = Simulation{eval(type)}(atoms, model; DoFs=1)
-    z = Ensembles.select_u0(sim, v, r, 1, :diabatic)
-    SUITE["TullyModelOne"]["classical"][type] = @benchmarkable Dynamics.run_trajectory($z, $tspan, $sim)
+    sim = Simulation{eval(type)}(atoms, model)
+    z = DynamicsVariables(sim, v, r, SingleState(1))
+    SUITE["TullyModelOne"]["classical"][type] = @benchmarkable run_trajectory($z, $tspan, $sim; dt=1.0)
 end
 
 types = [:NRPMD, :FSSH, :Ehrenfest]
@@ -25,33 +25,7 @@ n_beads = 10
 r = fill(-5.0, 1, 1, n_beads)
 v = fill(8.9, 1, 1, n_beads) ./ atoms.masses[1]
 for type in types
-    sim = RingPolymerSimulation{eval(type)}(atoms, model, n_beads; DoFs=1)
-    z = Ensembles.select_u0(sim, v, r, 1, :diabatic)
-    SUITE["TullyModelOne"]["ring polymer"][type] = @benchmarkable Dynamics.run_trajectory($z, $tspan, $sim)
-end
-
-SUITE["DebyeSpinBoson"] = BenchmarkGroup()
-SUITE["DebyeSpinBoson"]["classical"] = BenchmarkGroup()
-SUITE["DebyeSpinBoson"]["ring polymer"] = BenchmarkGroup()
-
-model = DebyeSpinBoson(50)
-atoms = Atoms(ones(50))
-types = [:FSSH, :Ehrenfest]
-tspan = (0.0, 10.0)
-r = randn(1, 50)
-v = zeros(1, 50)
-for type in types
-    sim = Simulation{eval(type)}(atoms, model; DoFs=1)
-    z = Ensembles.select_u0(sim, v, r, 1, :diabatic)
-    SUITE["DebyeSpinBoson"]["classical"][type] = @benchmarkable Dynamics.run_trajectory($z, $tspan, $sim)
-end
-
-types = [:NRPMD, :FSSH, :Ehrenfest]
-n_beads = 10
-r = zeros(1, 50, n_beads)
-v = randn(1, 50, n_beads)
-for type in types
-    sim = RingPolymerSimulation{eval(type)}(atoms, model, n_beads; DoFs=1)
-    z = Ensembles.select_u0(sim, v, r, 1, :diabatic)
-    SUITE["DebyeSpinBoson"]["ring polymer"][type] = @benchmarkable Dynamics.run_trajectory($z, $tspan, $sim)
+    sim = RingPolymerSimulation{eval(type)}(atoms, model, n_beads)
+    z = DynamicsVariables(sim, v, r, SingleState(1))
+    SUITE["TullyModelOne"]["ring polymer"][type] = @benchmarkable run_trajectory($z, $tspan, $sim; dt=1.0)
 end
