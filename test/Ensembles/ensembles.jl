85d476c63172f115cdcb3b1ca7eaefe843fe8c30
James Gardner <james.gardner1421@gmail.com>
2021-11-08 12:34:04 +0000

    Finalise ensemble updates

--- test/Ensembles/ensembles.jl
+++ test/Ensembles/ensembles.jl
@@ -1,8 +1,7 @@
 using NonadiabaticMolecularDynamics
 using Test
-using RecursiveArrayTools
-using OrdinaryDiffEq
-using ComponentArrays
+using RecursiveArrayTools: ArrayPartition
+using ComponentArrays: ComponentVector
 
 atoms = Atoms([:H])
 model = NonadiabaticModels.Harmonic()
@@ -11,54 +10,24 @@ sim = Simulation(atoms, model)
 positions = [randn(1, length(atoms)) for i=1:10]
 velocities = [randn(1, length(atoms)) for i=1:10]
 distribution = DynamicalDistribution(positions, velocities, (1, 1))
-
-u = rand(distribution)
-prob = DynamicsMethods.create_problem(u, (0.0, 1.0), sim)
-@testset "OrderedSelection" begin 
-    selector = Ensembles.OrderedSelection(distribution, 1:10)
-    new_prob = selector(prob, 3, false)
-    u = NonadiabaticDistributions.pick(distribution, 3)
-    @test new_prob.u0 == ArrayPartition(u.v, u.r)
-    selector = Ensembles.OrderedSelection(distribution, 6:10)
-    new_prob = selector(prob, 3, false)
-    u = NonadiabaticDistributions.pick(distribution, 8)
-    @test new_prob.u0 == ArrayPartition(u.v, u.r)
-end
-
-@testset "RandomSelection" begin
-    selector = Ensembles.RandomSelection(distribution)
-    new_prob = selector(prob, 3, false)
-end
-
-@testset "SelectWithCallbacks" begin
-    selector = Ensembles.RandomSelection(distribution)
-    Ensembles.SelectWithCallbacks(selector, CallbackSet(), (:position,), 10)
-end
-
-@testset "SumReduction" begin
-    prob = ODEProblem((u,p,t) -> 1.01u, 0.5, (0.0,1.0))
-    prob_func(prob, i, repeat) = remake(prob,u0=rand())
-    output_func(sol,i) = last(sol), false
-    reduction = Ensembles.SumReduction()
-    # u_init = Ensembles.output_template(output_func)
-    ensemble = EnsembleProblem(prob, prob_func=prob_func, output_func=output_func, reduction=reduction)
-    sol = solve(ensemble, Tsit5(), trajectories=1e3, batch_size=20)
-    @test sol.u / 1e3 ≈ 0.5 * exp(1.01) rtol=1e-1
-end
-
-@testset "MeanReduction" begin
-    prob = ODEProblem((u,p,t) -> 1.01u, 0.5, (0.0,1.0))
-    prob_func(prob, i, repeat) = remake(prob,u0=rand())
-    output_func(sol, i) = last(sol), false
-    reduction = Ensembles.MeanReduction()
-    ensemble = EnsembleProblem(prob, prob_func=prob_func, output_func=output_func, reduction=reduction, u_init=0)
-    sol = solve(ensemble, Tsit5(), trajectories=1e3, batch_size=20)
-    @test sol.u ≈ 0.5 * exp(1.01) rtol=1e-1
-end
+tspan = (0.0, 10.0)
 
 @testset "run_trajectories" begin
-    tspan = (0.0, 10.0)
     out = Ensembles.run_trajectories(sim, tspan, distribution;
         output=(:position), dt=1, trajectories=10)
+    @test length(out) == 10
+    @test out[1].position isa Vector{<:Matrix}
+    @test out[1].t isa Vector{<:Real}
 end
 
+@testset "run_ensemble" begin
+    out = Ensembles.run_ensemble(sim, tspan, distribution;
+        output=Ensembles.OutputFinal(), dt=1, trajectories=10, reduction=:append)
+    @test out isa Vector{<:ArrayPartition}
+end
+
+@testset "run_ensemble, reduction=$reduction" for reduction ∈ (:sum, :mean)
+    out = Ensembles.run_ensemble(sim, tspan, distribution;
+        output=Ensembles.OutputFinal(), dt=1, trajectories=10, reduction=reduction)
+    @test out isa ComponentVector
+end
