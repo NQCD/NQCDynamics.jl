da12492d6905c9961cf9019f234ab84b27b70a6b
James Gardner <38594562+jamesgardner1421@users.noreply.github.com>
2022-10-07 13:31:59 +0100

    Use FastLapackInterface for FastDeterminant (#14)

--- test/Core/FastDeterminant.jl
+++ test/Core/FastDeterminant.jl
@@ -2,12 +2,13 @@
 using Test
 using NQCDynamics: FastDeterminant
 using LinearAlgebra: det
+using FastLapackInterface: LUWs
 
 n = 5
 
 @testset "det! $T" for T in (Float32, Float64, ComplexF32, ComplexF64)
     A1 = rand(T, n, n)
     A2 = copy(A1)
-    ipiv = zeros(Int, n)
-    @test FastDeterminant.det!(A1, ipiv) ≈ det(A2)
+    ws = LUWs(A1)
+    @test FastDeterminant.det!(A1, ws) ≈ det(A2)
 end
