a3edc89051536ed8fd8259aad7c6715ec8733396
Louhokseson <Xuexun.Lu@warwick.ac.uk>
2023-11-30 17:31:54 +0000

    added comments

--- src/DynamicsMethods/ClassicalMethods/diabatic_mdef.jl
+++ src/DynamicsMethods/ClassicalMethods/diabatic_mdef.jl
@@ -14,7 +14,12 @@ end
 function DiabaticMDEF(masses::AbstractVector, DoFs::Integer, friction_method)
     DiabaticMDEF(get_mass_scale_matrix(masses, DoFs), friction_method)
 end
+"""
+    当你在生成 simulation 时，method 选择了 DiabaticMDEF
 
+    电脑会选择跑下面这段代码
+
+"""
 function NQCDynamics.Simulation{DiabaticMDEF}(atoms::Atoms, model::Model;
     friction_method, kwargs...
 )
