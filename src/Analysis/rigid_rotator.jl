364e3c21333fdf7b7b91636c021637bba018b8fd
Alexander Spears (Mobile) <alexander.spears@tum.de>
2024-05-22 10:11:53 +0100

    More function definition syntax mistakes

--- src/Analysis/rigid_rotator.jl
+++ src/Analysis/rigid_rotator.jl
@@ -8,7 +8,7 @@ using LinearAlgebra: norm
 
 Returns the classical translational energy in Hartree
 """
-function classical_translational_energy(config::Any, ind1::Union{Int,CartesianIndex}, ind2::Union{Int,CartesianIndex}, sim <: AbstractSimulation)
+function classical_translational_energy(config::Any, ind1::Union{Int,CartesianIndex}, ind2::Union{Int,CartesianIndex}, sim :: AbstractSimulation)
     return sum(sim.atoms.masses[[ind1, ind2]]) / 2 * norm(velocity_center_of_mass(config, ind1, ind2, sim.atoms))^2
 end
 
@@ -17,7 +17,7 @@ end
 
 Classical rotation energy of a rigid diatomic rotor in Hartree
 """
-function classical_rotation_energy(J::Int, config::Any, ind1::Union{Int,CartesianIndex}, ind2::Union{Int,CartesianIndex}, sim <: AbstractSimulation)
+function classical_rotation_energy(J::Int, config::Any, ind1::Union{Int,CartesianIndex}, ind2::Union{Int,CartesianIndex}, sim :: AbstractSimulation)
     I = reduced_mass(sim, ind1, ind2) * austrip(pbc_distance(config, ind1, ind2, sim))^2 # Classical moment of inertia in atomic units.
     return J * (J + 1) / (2 * I)
 end
@@ -27,7 +27,7 @@ end
 
 Vibrational energy of a harmonic oscillator with the force constant k and vibrational level ν.
 """
-function harmonic_vibration_energy(ν::Union{Int}, k::Float64, ind1::Union{Int,CartesianIndex}, ind2::Union{Int,CartesianIndex}, sim <: AbstractSimulation)
+function harmonic_vibration_energy(ν::Union{Int}, k::Float64, ind1::Union{Int,CartesianIndex}, ind2::Union{Int,CartesianIndex}, sim :: AbstractSimulation)
     return (ν + 1 / 2) * sqrt(k / reduced_mass(sim, ind1, ind2))
 end
 
