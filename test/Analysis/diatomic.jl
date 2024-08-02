9a8c85d67a26138b81ca2f58762b0124b7a26f08
Alexander Spears <alexander.spears@warwick.ac.uk>
2024-08-02 09:34:22 +0100

    PyCall -> PythonCall in tests

--- test/Analysis/diatomic.jl
+++ test/Analysis/diatomic.jl
@@ -1,8 +1,9 @@
 using Test
+using PythonCall
 using NQCDynamics
 using Unitful, UnitfulAtomic
-using PyCall
 using NQCModels
+using NQCBase
 using JLD2
 
 @testset "Desorption Analysis" begin
@@ -10,7 +11,7 @@ using JLD2
     desorption_trajectory_ase=ase_io.read("artifacts/desorption_test.xyz", index=":")
     desorption_dynamicsvariables=jldopen("artifacts/desorption_dynamics.jld2")["trajectory"]
     # Make a dummmy simulation out of the first structure (we don't need a working potential to test. )
-    atoms, initial_positions, cell=NQCDynamics.convert_from_ase_atoms(desorption_trajectory_ase[1])
+    atoms, initial_positions, cell=convert_from_ase_atoms(desorption_trajectory_ase[1])
     simulation=Simulation(atoms, AdiabaticASEModel(desorption_trajectory_ase[1]), cell=cell)
     diatomic_indices=[55,56]
     @test Analysis.Diatomic.get_desorption_frame(desorption_dynamicsvariables, diatomic_indices, simulation; surface_distance_threshold=2.4u"Å") == 2675
