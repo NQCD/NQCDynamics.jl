ae02e34008911a57e30a44d388b523cf9695cdc2
Alexander Spears <alexander.spears@warwick.ac.uk>
2024-10-02 11:04:29 +0100

    Add DynamicalDistribution constructor from Simulation, add test for correct atom freezing.

--- src/NQCDistributions-convenience.jl
+++ src/NQCDistributions-convenience.jl
@@ -1,5 +1,5 @@
 """
-Add-on functions for NQCDistributions to make generating distributions more convenient. 
+Add-on functions for NQCDistributions to make generating distributions more convenient.
 
 """
 
@@ -8,21 +8,28 @@ import Distributions
 """
     VelocityBoltzmann(temperature, sim::AbstractSimulation; center = zeros(size(sim)))
 
-Generates a VelocityBoltzmann distribution covering an entire Simulation. 
-If `NQCModels.mobileatoms` is modified, velocities for immobile atoms will always be zero. 
+Generates a VelocityBoltzmann distribution covering an entire Simulation.
+If `NQCModels.mobileatoms` is modified, velocities for immobile atoms will always be zero.
 """
-function NQCDistributions.VelocityBoltzmann(temperature, sim::AbstractSimulation; center = zeros(Float64, size(sim)))
-	if length(NQCModels.mobileatoms(sim))==size(sim)[2]
-		# Simple case: All atoms should be moving, so give them all Boltzmann distributions. 
-		return NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses, size(sim); center = center)
-	else
-		# Not all atoms should be moving, so assign Boltzmann distribution only to mobile atoms. 
-		sampleable_array=convert(Matrix{Distributions.UnivariateDistribution}, hcat([[Distributions.Dirac(center[i,j]) for i in 1:size(sim)[1]] for j in 1:size(sim)[2]]...))
-		for i in NQCModels.mobileatoms(sim)
-			for j in NQCModels.dofs(sim)
-				sampleable_array[j,i]=NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses[i]; center=center[j,i])
-			end
-		end
-		return NQCDistributions.UnivariateArray(sampleable_array)
-	end
+function NQCDistributions.VelocityBoltzmann(temperature, sim::AbstractSimulation; center=zeros(Float64, size(sim)))
+    if length(NQCModels.mobileatoms(sim)) == size(sim)[2]
+        # Simple case: All atoms should be moving, so give them all Boltzmann distributions.
+        return NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses, size(sim); center=center)
+    else
+        # Not all atoms should be moving, so assign Boltzmann distribution only to mobile atoms.
+        sampleable_array = convert(Matrix{Distributions.UnivariateDistribution}, hcat([[Distributions.Dirac(center[i, j]) for i in 1:size(sim)[1]] for j in 1:size(sim)[2]]...))
+        for i in NQCModels.mobileatoms(sim)
+            for j in NQCModels.dofs(sim)
+                sampleable_array[j, i] = NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses[i]; center=center[j, i])
+            end
+        end
+        return NQCDistributions.UnivariateArray(sampleable_array)
+    end
+end
+
+function NQCDistributions.DynamicalDistribution(velocities, positions, simulation::Simulation; classical=Int[])
+    mobile_atoms = NQCModels.mobileatoms(simulation.calculator.model, 1)
+    simulation_dims = size(simulation)
+    frozen_indices = symdiff(1:simulation_dims[2], mobile_atoms)
+    return NQCDistributions.DynamicalDistribution(velocities, positions, simulation_dims; frozen_atoms=frozen_indices, classical=classical)
 end
--- test/Core/simulations.jl
+++ test/Core/simulations.jl
@@ -27,7 +27,7 @@ model = NQCModels.Free()
 @testset "get_temperature" begin
     sim = Simulation(atoms, model; temperature=10u"K")
     @test NQCDynamics.get_temperature(sim) isa Real
-    sim = Simulation(atoms, model; temperature=x->10u"K")
+    sim = Simulation(atoms, model; temperature=x -> 10u"K")
     @test NQCDynamics.get_temperature(sim) isa Real
     sim = Simulation(atoms, model; temperature=10)
     @test NQCDynamics.get_temperature(sim, 1) isa Real
@@ -36,7 +36,7 @@ end
 @testset "get_ring_polymer_temperature" begin
     sim = RingPolymerSimulation(atoms, model, 10; temperature=10u"K")
     @test NQCDynamics.get_ring_polymer_temperature(sim) isa Real
-    sim = RingPolymerSimulation(atoms, model, 10; temperature=x->10u"K")
+    sim = RingPolymerSimulation(atoms, model, 10; temperature=x -> 10u"K")
     @test NQCDynamics.get_ring_polymer_temperature(sim) isa Real
     sim = RingPolymerSimulation(atoms, model, 10; temperature=10)
     @test NQCDynamics.get_ring_polymer_temperature(sim, 1) isa Real
@@ -81,13 +81,18 @@ end
     nqcd_atoms, nqcd_positions, nqcd_cell = NQCDynamics.convert_from_ase_atoms(structure)
     # Test the frozen model
     frozen_model = AdiabaticASEModel(structure)
-    frozen_sim=Simulation(nqcd_atoms, frozen_model; cell=cell, temperature=10.0)
+    frozen_sim = Simulation(nqcd_atoms, frozen_model; cell=cell, temperature=10.0)
 
     # Pass: Atoms 1-18 are frozen, 19-56 are mobile
     @test NQCModels.mobileatoms(frozen_sim) == collect(19:56)
     test_dist = NQCDistributions.VelocityBoltzmann(10, frozen_sim)
     # Pass: First DOF of first atom should be frozen, so Dirac distribution
-    findfirst(x->isa(x, Distributions.Dirac), test_dist.sampleable) == CartesianIndex(1,1)
+    findfirst(x -> isa(x, Distributions.Dirac), test_dist.sampleable) == CartesianIndex(1, 1)
     # Pass: First DOF of Atom 19 should be mobile, so Normal distribution
-    findfirst(x->isa(x, Distributions.Normal), test_dist.sampleable) == CartesianIndex(1,19)
+    findfirst(x -> isa(x, Distributions.Normal), test_dist.sampleable) == CartesianIndex(1, 19)
+
+    # Test full distribution generation
+    test_dist = NQCDistributions.DynamicalDistribution(fill(3.0, size(nqcd_positions)), nqcd_positions, frozen_sim)
+    dist_random = rand(test_dist)
+    @test all(dist_random.v[:, 1:18] .== 0.0) # Velocities should be zero for frozen atoms.
 end
