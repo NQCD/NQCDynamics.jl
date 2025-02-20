ce7f9114ee2268efbfa6994a2bd0b08c168364c0
Ash Baldwin <ash.baldwin@warwick.ac.uk>
2025-02-20 20:35:56 +0000

    Moving general surface hopping functions to SurfaceHoppingMethods.jl (#357)
    * changed location of get_hopping_eigenvalues()
    
    changed the location of the definition of the get_hopping_eigenvalues() function to SurfaceHoppingMethods.jl so this general surface hopping function is defined in a more appropriate location
    
    * fixing mistake in previous commit
    
    Previous commit copied over the wrong function from SurfaceHoppingMethods\rpsh.jl
    
    * added RingPolymerSimulation as dependency in SurfaceHoppingMethods.jl
    
    * Moved general surface hopping functions to SurfaceHoppingMethods.jl (#356)
    
    Moved two generally applicable surface hopping functions get_hopping_nonadiabatic_coupling() and get_hopping_velocity() to SurfaceHoppingMethods.jl and moved their duplicate RingPolymer definitions to the same file to bring these definitions more in line with the duplicate function definition philosophy in the rest of NQCD
    
    * Imported get_centroid() to SurfaceHoppingMethods
    
    Added get_centroid() to SurfaceHoppingMethods.jl so that get_hopping_velocity() is able to work as expected.

--- src/DynamicsMethods/SurfaceHoppingMethods/SurfaceHoppingMethods.jl
+++ src/DynamicsMethods/SurfaceHoppingMethods/SurfaceHoppingMethods.jl
@@ -11,6 +11,7 @@ using ComponentArrays: ComponentVector
 using DiffEqBase: DiffEqBase
 using LinearAlgebra: LinearAlgebra, lmul!
 using OrdinaryDiffEq: OrdinaryDiffEq
+using RingPolymerArrays: get_centroid
 
 using NQCDynamics:
     NQCDynamics,
@@ -91,6 +92,22 @@ end
 
 function DynamicsUtils.get_hopping_eigenvalues(sim::RingPolymerSimulation, r::AbstractArray{T,3}) where {T}
     return Calculators.get_centroid_eigen(sim.calculator, r).values
+end
+
+function DynamicsUtils.get_hopping_nonadiabatic_coupling(sim::Simulation, r::AbstractMatrix)
+    return Calculators.get_nonadiabatic_coupling(sim.calculator, r)
+end
+
+function DynamicsUtils.get_hopping_nonadiabatic_coupling(sim::RingPolymerSimulation, r::AbstractArray{T,3}) where {T}
+    return Calculators.get_centroid_nonadiabatic_coupling(sim.calculator, r)
+end
+
+function DynamicsUtils.get_hopping_velocity(::Simulation, v::AbstractMatrix)
+    return v
+end
+
+function DynamicsUtils.get_hopping_velocity(::RingPolymerSimulation, v::AbstractArray{T,3}) where {T}
+    return get_centroid(v)
 end
 
 include("decoherence_corrections.jl")
