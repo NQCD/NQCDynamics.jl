9ee23803d89060101c87c9b6745d0ba5fe6c89b8
Alexander Spears <alexander.spears@warwick.ac.uk>
2025-04-29 17:03:11 +0100

    🪲 Another method mistake

--- src/NQCDistributions-convenience.jl
+++ src/NQCDistributions-convenience.jl
@@ -14,13 +14,13 @@ Generates a VelocityBoltzmann distribution covering an entire Simulation.
 If `NQCModels.mobileatoms` is modified, velocities for immobile atoms will always be zero.
 """
 function NQCDistributions.VelocityBoltzmann(temperature, sim::AbstractSimulation; center=zeros(Float64, size(sim)))
-    if length(NQCModels.mobileatoms(sim, 1)) == size(sim)[2]
+    if length(NQCModels.mobileatoms(sim)) == size(sim)[2]
         # Simple case: All atoms should be moving, so give them all Boltzmann distributions.
         return NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses, size(sim); center=center)
     else
         # Not all atoms should be moving, so assign Boltzmann distribution only to mobile atoms.
         sampleable_array = convert(Matrix{Distributions.UnivariateDistribution}, hcat([[Distributions.Dirac(center[i, j]) for i in 1:size(sim)[1]] for j in 1:size(sim)[2]]...))
-        for i in NQCModels.mobileatoms(sim, 1)
+        for i in NQCModels.mobileatoms(sim)
             for j in NQCModels.dofs(sim)
                 sampleable_array[j, i] = NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses[i]; center=center[j, i])
             end
