fb58f0c0d9e022cefc457c0219924c1a28aea162
Alexander Spears <alexander.spears@warwick.ac.uk>
2025-04-29 16:59:58 +0100

    🐛 Logic issues constructing DynamicalDistribution

--- src/NQCDistributions-convenience.jl
+++ src/NQCDistributions-convenience.jl
@@ -14,13 +14,13 @@ Generates a VelocityBoltzmann distribution covering an entire Simulation.
 If `NQCModels.mobileatoms` is modified, velocities for immobile atoms will always be zero.
 """
 function NQCDistributions.VelocityBoltzmann(temperature, sim::AbstractSimulation; center=zeros(Float64, size(sim)))
-    if length(NQCModels.mobileatoms(sim)) == size(sim)[2]
+    if length(NQCModels.mobileatoms(sim, 1)) == size(sim)[2]
         # Simple case: All atoms should be moving, so give them all Boltzmann distributions.
         return NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses, size(sim); center=center)
     else
         # Not all atoms should be moving, so assign Boltzmann distribution only to mobile atoms.
         sampleable_array = convert(Matrix{Distributions.UnivariateDistribution}, hcat([[Distributions.Dirac(center[i, j]) for i in 1:size(sim)[1]] for j in 1:size(sim)[2]]...))
-        for i in NQCModels.mobileatoms(sim)
+        for i in NQCModels.mobileatoms(sim, 1)
             for j in NQCModels.dofs(sim)
                 sampleable_array[j, i] = NQCDistributions.VelocityBoltzmann(temperature, sim.atoms.masses[i]; center=center[j, i])
             end
@@ -33,6 +33,10 @@ function NQCDistributions.DynamicalDistribution(velocities, positions, simulatio
     mobile_atoms = NQCModels.mobileatoms(simulation.calculator.model, 1)
     simulation_dims = size(simulation)
     frozen_indices = symdiff(1:simulation_dims[2], mobile_atoms)
-    return NQCDistributions.DynamicalDistribution(velocities, positions, simulation_dims; frozen_atoms=frozen_indices, classical=classical)
+    if isempty(classical)
+        return NQCDistributions.DynamicalDistribution(velocities, positions, simulation_dims; frozen_atoms=frozen_indices)
+    else
+        return NQCDistributions.DynamicalDistribution(velocities, positions, simulation_dims; frozen_atoms=frozen_indices, classical = classical)
+    end
 
 end
