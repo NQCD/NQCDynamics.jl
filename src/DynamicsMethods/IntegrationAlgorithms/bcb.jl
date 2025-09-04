41c0f6822172305d49182d7ef6c7324055613d7e
Matt Larkin <61248744+mlarkin863@users.noreply.github.com>
2025-09-04 09:09:18 +0100

    commented versions of verify_f2 with and without OrdinaryDiffEqSympleticRK - easiser switching between v6.66.0 and v6.87.0

--- src/DynamicsMethods/IntegrationAlgorithms/bcb.jl
+++ src/DynamicsMethods/IntegrationAlgorithms/bcb.jl
@@ -23,11 +23,11 @@ function OrdinaryDiffEq.alg_cache(::BCB,u,rate_prototype,::Type{uEltypeNoUnits},
     BCBCache(u, uprev, k, tmp, fsalfirst, halfdt, cayley)
 end
 
+#### OrdinaryDiffEq v6.87.0
 function OrdinaryDiffEq.OrdinaryDiffEqSymplecticRK.verify_f2(f, res, p, q, pa, t, integrator, ::BCBCache)
     f(res, p, q, pa, t)
     res == p ? res : OrdinaryDiffEq.throwex(integrator)
 end
-
 
 function OrdinaryDiffEq.initialize!(integrator, integrator_cache::BCBCache)
     integrator.fsalfirst = integrator_cache.fsalfirst
@@ -42,6 +42,27 @@ function OrdinaryDiffEq.initialize!(integrator, integrator_cache::BCBCache)
     integrator.f.f1(integrator.k[2].x[1],duprev,uprev,integrator.p,integrator.t)
     OrdinaryDiffEq.OrdinaryDiffEqSymplecticRK.verify_f2(integrator.f.f2, integrator.k[2].x[2], duprev, uprev, integrator.p, integrator.t, integrator, integrator_cache)
 end
+
+# #### OrdinaryDiffEq v6.66.0
+# function OrdinaryDiffEq.verify_f2(f, res, p, q, pa, t, integrator, ::BCBCache)
+#     f(res, p, q, pa, t)
+#     res == p ? res : OrdinaryDiffEq.throwex(integrator)
+# end
+
+
+# function OrdinaryDiffEq.initialize!(integrator, integrator_cache::BCBCache)
+#     integrator.fsalfirst = integrator_cache.fsalfirst
+#     integrator.fsallast = integrator_cache.k
+  
+#     integrator.kshortsize = 2
+#     resize!(integrator.k, integrator.kshortsize)
+#     integrator.k[1] = integrator.fsalfirst
+#     integrator.k[2] = integrator.fsallast
+  
+#     duprev,uprev = integrator.uprev.x
+#     integrator.f.f1(integrator.k[2].x[1],duprev,uprev,integrator.p,integrator.t)
+#     OrdinaryDiffEq.verify_f2(integrator.f.f2, integrator.k[2].x[2], duprev, uprev, integrator.p, integrator.t, integrator, integrator_cache)
+# end
 
 @muladd function OrdinaryDiffEq.perform_step!(integrator, integrator_cache::BCBCache, repeat_step=false)
     @unpack t, dt, p = integrator
