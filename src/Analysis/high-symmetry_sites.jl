d96a571f9352c2d4a39e92d0c06c8bc2b3337a73
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2025-11-10 12:06:02 +0100

    🥅 Add debug printing
    (cherry picked from commit f1861c4aceeb38be00be16a3e509a914038c73ba)

--- src/Analysis/high-symmetry_sites.jl
+++ src/Analysis/high-symmetry_sites.jl
@@ -123,6 +123,7 @@ function positions_to_category(
 			end
 		end
 	end
+  @debug "Site distances:" sites = category_names distances = site_distances
 	if any(site_distances .≤ snap_to_site) # Check if any category is sufficiently close
 		return category_names[argmin(site_distances)] # Return the classified category
 	else
