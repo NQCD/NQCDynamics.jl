b8c57082e8b60df34d50a7296f761b3ce74456ed
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2026-06-01 13:41:32 +0200

    🐞 Fix diatomic transform (#465)
    * 🧪 Add unit test for internal coordinate transform
    
    * 🧪 Fix typo and loop testing
    
    * 🧪 Another typo
    
    * Update tests for NQCBase#31

--- test/Analysis/diatomic.jl
+++ test/Analysis/diatomic.jl
@@ -4,6 +4,7 @@ using Unitful, UnitfulAtomic
 using PythonCall
 using NQCModels
 using NQCDInterfASE
+using LinearAlgebra
 using JLD2
 
 @testset "Desorption Analysis" begin
@@ -16,4 +17,33 @@ using JLD2
     diatomic_indices=[55,56]
     @test Analysis.Diatomic.get_desorption_frame(desorption_dynamicsvariables, diatomic_indices, simulation; surface_distance_threshold=austrip(2.4u"Å")) == 2675
     @test Analysis.Diatomic.get_desorption_angle(desorption_dynamicsvariables, diatomic_indices, simulation; surface_distance_threshold=austrip(2.4u"Å")) ≈ 28.05088202518
+end
+
+@testset "Internal coordinate transformation" begin
+    structure_import = NQCDynamics.read_extxyz("artifacts/desorption_test.xyz")
+    # Take only the first structure
+    first_structure = first(structure.import)
+    # Generate a random (symmetric) friction tensor
+    eft = Symmetric(rand(6,6)) |> Matrix
+    diatomic_indices = [55,56]
+    simulation=Simulation(first_structure.atoms, Free(), cell=first_structure.cell)
+    # Ensure internal coordinate transformation is trace-conserving
+    eft_internal = NQCDynamics.Analysis.Diatomic.transform_to_internal_coordinates(
+        eft,
+        first_structure.positions,
+        diatomic_indices...,
+        simulation
+    )
+    @test isapprox(eft |> diag |> sum, eft_internal |> diag |> sum)
+    # Ensure doing the back-transform on the forward transform gives back the same matrix. 
+    eft_cartesian_again = NQCDynamics.Analysis.Diatomic.transform_from_internal_coordinates(
+        eft_internal,
+        first_structure.positions,
+        diatomic_indices...,
+        simulation
+    )
+    matrix_comparison = isapprox.(eft, eft_cartesian_again)
+    for idx in eachindex(matrix_comparison)
+        @test matrix_comparison[idx]
+    end
 end
